4ef9035cf8ff987c58c494b223f89d4568896500
n-ando <Noriaki.Ando@gmail.com>
2005-05-12 09:06:24 +0000

    Public release.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/AIST@4 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ rtm/RtcFactory.cpp
@@ -0,0 +1,53 @@
+// -*- C++ -*-
+/*!
+ * @file RtcFactory.h
+ * @brief RT component manager class
+ * @date $Date: 2005-05-12 09:06:18 $
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2003-2005
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id: RtcFactory.cpp,v 1.1.1.1 2005-05-12 09:06:18 n-ando Exp $
+ *
+ */
+
+/*
+ * $Log: not supported by cvs2svn $
+ *
+ */
+
+#include "rtm/RtcBase.h"
+#include "rtm/RtcModuleProfile.h"
+#include "rtm/RtcManager.h"
+#include "rtm/RtcFactory.h"
+
+namespace RTM 
+{
+
+
+  RtcFactoryCXX::RtcFactoryCXX(RtcModuleProfile profile,
+			       RtcNewFunc new_func,
+			       RtcDeleteFunc delete_func)
+    : RtcFactoryBase(profile), m_New(new_func), m_Delete(delete_func)
+  {
+  }
+
+
+  RtcBase* RtcFactoryCXX::create(RtcManager* mgr)
+  {
+    ++m_Number;
+    return m_New(mgr);
+  }
+
+  void RtcFactoryCXX::destroy(RtcBase* comp)
+  {
+    m_Delete(comp);
+  }
+
+};
+
