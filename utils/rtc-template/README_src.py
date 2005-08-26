6ddd00e2a987eea89b30f5bd2bf3546af58871fd
n-ando <Noriaki.Ando@gmail.com>
2005-08-26 12:01:15 +0000

    Now rtc-template uses ezt (Easy Template).
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@47 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- utils/rtc-template/README_src.py
+++ utils/rtc-template/README_src.py
@@ -3,7 +3,7 @@
 #
 #  @file README_src.py
 #  @brief rtc-template RTComponent's README & specification file generator class
-#  @date $Date: 2005-05-12 09:06:18 $
+#  @date $Date: 2005-08-26 12:01:15 $
 #  @author Noriaki Ando <n-ando@aist.go.jp>
 # 
 #  Copyright (C) 2004-2005
@@ -13,182 +13,141 @@
 #          Advanced Industrial Science and Technology (AIST), Japan
 #      All rights reserved.
 # 
-#  $Id: README_src.py,v 1.1.1.1 2005-05-12 09:06:18 n-ando Exp $
+#  $Id: README_src.py,v 1.2 2005-08-26 12:01:15 n-ando Exp $
 # 
 
 #
 #  $Log: not supported by cvs2svn $
+#  Revision 1.1.1.1  2005/05/12 09:06:18  n-ando
+#  Public release.
+#
 #
 
 import re
 import os
 import time
+import ezt
+import gen_base
 
-class README_src:
-	def __init__(self, fname, spec):
-		self.fname = "README." + fname
-		self.spec = spec
-		self.version = os.popen("rtm-config --version", "r").read()
-		
-	def print_readme(self):
-		if (not self.check_overwrite(self.fname)):
-			return
-		readme = file(self.fname, "w")
+readme = """======================================================================
+  RTComponent: [module.name] specificatioin
 
-		print >> readme, """======================================================================
-  RTComponent: %s specificatioin
-
-  OpenRTM-%s
-  Date: %s
+  OpenRTM-[version]
+  Date: [date]
 ======================================================================
-""" % (self.spec["module-name"][1],
-	   self.version,
-	   time.asctime())
-		
-		print >> readme, """Module Name: %s
-Description: %s
-Version:     %s
-Author:      %s
-Category:    %s
-Comp. Type:  %s
-Act. Type:   %s
-MAX Inst.:   %s
-Lang:        %s
-Lang Type:   %s
-""" % (self.spec["module-name"][1],
-	   self.spec["module-desc"][1],
-	   self.spec["module-version"][1],
-	   self.spec["module-author"][1],
-	   self.spec["module-category"][1],
-	   self.spec["module-comp-type"][1],
-	   self.spec["module-act-type"][1],
-	   self.spec["module-max-inst"][1],
-	   self.spec["module-lang"][1],
-	   self.spec["module-lang"][1])
+
+Description: [module.desc]
+Version:     [module.version]
+Author:      [module.author]
+Category:    [module.category]
+Comp. Type:  [module.comp_type]
+Act. Type:   [module.act_type]
+MAX Inst.:   [module.max_inst]
+Lang:        
+Lang Type:   
 
 
-		print >> readme, """======================================================================"""
-		print >> readme, """    Activity definition"""
-		print >> readme, """======================================================================"""
+======================================================================
+    Activity definition
+======================================================================
 
-		print >> readme, """
-[Initializing]
+[b]Initializing[e]
 
-[Ready]
-	[Entry]
-	[Do]
-	[Exit]
+[b]Ready[e]
+	[b]Entry[e]
+	[b]Do[e]
+	[b]Exit[e]
 
-[Starting]
+[b]Starting[e]
 
-[Active]
-	[Entry]
-	[Do]
-	[Exit]
+[b]Active[e]
+	[b]Entry[e]
+	[b]Do[e]
+	[b]Exit[e]
 
-[Stopping]
+[b]Stopping[e]
 
-[Error]
-	[Entry]
-	[Do]
-	[Exit]
+[b]Error[e]
+	[b]Entry[e]
+	[b]Do[e]
+	[b]Exit[e]
 
 
-[Fatal Error]
-	[Entry]
-	[Do]
-	[Exit]
+[b]Fatal Error[e]
+	[b]Entry[e]
+	[b]Do[e]
+	[b]Exit[e]
 
-[Exiting]
+[b]Exiting[e]
+
+======================================================================
+    InPorts definition
+======================================================================
+[for inport]
+	Name:        [inport.name]
+	PortNumber:  [inport.num]
+	Description:
+	PortType: 
+	DataType:    [inport.type]
+	MaxOut: 
+	[b]Data Elements[e]
+		Number:
+		Name:
+		Type:
+		Unit:
+		RangeLow:
+		RangeHigh:
+		DefaultValue:
+[end]
+
+======================================================================
+    OutPorts definition
+======================================================================
+[for outport]
+	Name:        [outport.name]
+	PortNumber:  [outport.num]
+	Description:
+	PortType: 
+	DataType:    [outport.type]
+	MaxOut: 
+	[b]Data Elements[e]
+		Number:
+		Name:
+		Type:
+		Unit:
+		RangeLow:
+		RangeHigh:
+		DefaultValue:
+[end]
+
+======================================================================
+    Configuration definition
+======================================================================
+This area is reserved for future OpenRTM.
 
 """
 
-		print >> readme, """======================================================================"""
-		print >> readme, """    InPorts definition"""
-		print >> readme, """======================================================================"""
-		i = 0
-		for inpname in self.spec["module-inport"][1]:
-			inp = self.spec["module-inport"][1][inpname]
-			print >> readme, """[InPort]
-	Name:        %s
-	PortNumber:  %d
-	Description:
-	PortType: 
-	DataType:    %s
-	MaxOut: 
-	[Data Elements]
-		Number:
-		Name:
-		Type:
-		Unit:
-		RangeLow:
-		RangeHigh:
-		DefaultValue:
-
-		""" % (inpname, i, inp)
-			i += 1
-
-		print >> readme, """======================================================================"""
-		print >> readme, """    OutPorts definition"""
-		print >> readme, """======================================================================"""
-		i = 0
-		for outpname in self.spec["module-outport"][1]:
-			outp = self.spec["module-outport"][1][outpname]
-			print >> readme, """[OutPort]
-	Name:        %s
-	PortNumber:  %d
-	Description:
-	PortType: 
-	DataType:    %s
-	MaxOut: 
-	[Data Elements]
-		Number:
-		Name:
-		Type:
-		Unit:
-		RangeLow:
-		RangeHigh:
-		DefaultValue:
-
-		""" % (inpname, i, inp)
-			i += 1
-
-		print >> readme, """======================================================================"""
-		print >> readme, """    Configuration definition"""
-		print >> readme, """======================================================================"""
-		print >> readme, """This area is reserved for future OpenRTM."""
-		
-		print self.fname, "was generated"
-
-	def check_overwrite(self, fname):
-		if (os.access(fname, os.F_OK)):
-			ans = raw_input("\"" + fname + "\" already exists. Overwrite? (y/n) ")
-			if (ans == "y" or ans == "Y"):
-				return True
-			else:
-				return False
-		else:
-			return True
-
-def main():
-	module_spec = {"module-name":["RTC_MODULE_NAME","Hoge"],
-			   "module-desc":["RTC_MODULE_DESC","Hoge component"],
-			   "module-version":["RTC_MODULE_VERSION","0.1"],
-			   "module-author":["RTC_MODULE_AUTHOR","Hoge"],
-			   "module-category":["RTC_MODULE_CATEGORY","Generic"],
-			   "module-comp-type":["RTC_MODULE_COMP_TYPE","COMMUTATIVE"],
-			   "module-act-type":["RTC_MODULE_ACT_TYPE","SPORADIC"],
-			   "module-max-inst":["RTC_MODULE_MAX_INST","10"],
-			   "module-lang":["RTC_MODULE_LANG","C++"],
-			   "module-inport":["", {"Ref":"TimedFloat", \
-									"Sens":"TimedDouble"}],
-			   "module-outport":["", {"Ctrl":"TimedFloat", \
-									 "Monitor":"TimedShort"}]
-			   }
-
-	readme = README_src("HogeTest", module_spec)
-	readme.print_readme()
 
 
-if __name__ == "__main__":
-	main()
+class README_src(gen_base.gen_base):
+	def __init__(self, data):
+		self.data = data.copy()
+		self.data['fname'] = "README." + self.data['fname']
+		self.data['version'] = os.popen("rtm-config --version", "r").read()
+		self.data['date'] = time.asctime()
+		self.data['b'] = "["
+		self.data['e'] = "]"
+		return
+
+	def print_readme(self):
+		f = self.check_overwrite(self.data["fname"])
+		if (not f):
+			return
+		t = ezt.Template(compress_whitespace = 0)
+		t.parse(readme)
+		t.generate(f, self.data)
+		print self.data["fname"], " was generated."
+
+	def print_all(self):
+		self.print_readme()
+	
