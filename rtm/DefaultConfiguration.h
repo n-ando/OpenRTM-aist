d81ed3902662a0e13a24fab73803cdb795db864b
n-ando <Noriaki.Ando@gmail.com>
2006-10-17 19:30:45 +0000

    The first comitment.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@124 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ rtm/DefaultConfiguration.h
@@ -0,0 +1,47 @@
+// -*- C++ -*-
+/*!
+ * @file DefaultConfiguration.h
+ * @brief RTC manager default configuration
+ * @date $Date: 2006-10-17 19:30:45 $
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2006
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id: DefaultConfiguration.h,v 1.1 2006-10-17 19:30:45 n-ando Exp $
+ *
+ */
+
+/*
+ * $Log: not supported by cvs2svn $
+ *
+ */
+
+static const char* default_config[] =
+  {
+    "config.version",      "0.4.0",
+    "manager.os.name",     "",
+    "manager.os.release",  "",
+    "manager.os.version",  "",
+    "manager.os.arch",     "",
+    "manager.os.hostname", "",
+    "manager.pid",         "",
+    "manager.logger.file", "",
+    "manager.logger.path", "",
+    "manager.logger.master_logger", "",
+    "manager.logger.master_logger_name", "",
+    "manager.logger.time_format", "",
+    "manager.module.conf_path", "",
+    "manager.module.load_path", "",
+    "manager.name_service", "CORBA",
+    "menager.name_server", "localhost",
+    "manager.name_format", "%H/%M/%m",
+    "manager.orb.args", "",
+    "manager.orb.addr_port", "",
+    "manager.orb.id", "",
+  };
+
