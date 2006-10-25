a40b9bc3ce942936f39efed6939973288336e274
n-ando <Noriaki.Ando@gmail.com>
2006-10-25 17:36:00 +0000

    Classes were renamed, and class reference manual was described.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@146 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/Factory.cpp
+++ rtm/Factory.cpp
@@ -1,8 +1,8 @@
 // -*- C++ -*-
 /*!
- * @file RtcFactory.h
+ * @file Factory.h
  * @brief RT component manager class
- * @date $Date: 2006-10-24 14:25:26 $
+ * @date $Date: 2006-10-25 17:35:52 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2003-2005
@@ -12,43 +12,67 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: Factory.cpp,v 1.2 2006-10-24 14:25:26 n-ando Exp $
+ * $Id: Factory.cpp,v 1.3 2006-10-25 17:35:52 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/10/24 14:25:26  n-ando
+ * Renamed RtcFactory.cpp to Factory.cpp
+ *
  * Revision 1.1.1.1  2005/05/12 09:06:18  n-ando
  * Public release.
  *
  *
  */
 
-#include "rtm/RtcBase.h"
-#include "rtm/RtcModuleProfile.h"
-#include "rtm/RtcManager.h"
-#include "rtm/RtcFactory.h"
+#include "rtm/Factory.h"
 
-namespace RTM 
+namespace RTC 
 {
 
+  FactoryBase::FactoryBase(Properties profile)
+    : m_Profile(profile), m_Number(-1)
+  {
+  }
 
-  RtcFactoryCXX::RtcFactoryCXX(RtcModuleProfile profile,
-			       RtcNewFunc new_func,
-			       RtcDeleteFunc delete_func)
-    : RtcFactoryBase(profile), m_New(new_func), m_Delete(delete_func)
+  FactoryBase::~FactoryBase()
   {
   }
 
 
-  RtcBase* RtcFactoryCXX::create(RtcManager* mgr)
+  Properties& FactoryBase::profile()
+  {
+    return m_Profile;
+  }
+
+
+  int FactoryBase::number()
+  {
+    return m_Number;
+  }
+
+
+  FactoryCXX::FactoryCXX(Properties profile,
+			 RtcNewFunc new_func,
+			 RtcDeleteFunc delete_func)
+    : FactoryBase(profile),
+      m_New(new_func),
+      m_Delete(delete_func)
+  {
+  }
+
+
+  RtcBase* FactoryCXX::create(Manager* mgr)
   {
     ++m_Number;
     return m_New(mgr);
   }
 
-  void RtcFactoryCXX::destroy(RtcBase* comp)
+  void FactoryCXX::destroy(RtcBase* comp)
   {
+    --m_Number;
     m_Delete(comp);
   }
 
