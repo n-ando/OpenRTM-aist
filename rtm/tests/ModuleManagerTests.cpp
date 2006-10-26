a7576cea34030854677181f1cf9d43dddd5197f1
kurihara <openrtm@openrtm.org>
2006-10-26 01:34:57 +0000

    test program of class ModuelManager.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@147 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/tests/ModuleManagerTests.cpp
+++ rtm/tests/ModuleManagerTests.cpp
@@ -1,24 +1,15 @@
 // -*- C++ -*-
 /*!
- * @file ModuleManagerTests.cpp
- * @brief ModuleManager class test
- * @date $Date: 2006-09-20 08:52:19 $
- * @author Noriaki Ando <n-ando@aist.go.jp>
- *
- * Copyright (C) 2006
- *     Task-intelligence Research Group,
- *     Intelligent Systems Research Institute,
- *     National Institute of
- *         Advanced Industrial Science and Technology (AIST), Japan
- *     All rights reserved.
- *
- * $Id: ModuleManagerTests.cpp,v 1.1 2006-09-20 08:52:19 n-ando Exp $
+ * @file 
+ * @brief Properties test class
+ * @date $Date: 2006-10-26 01:34:57 $
+ * @author Shinji Kurihara
+ * $Id: ModuleManagerTests.cpp,v 1.2 2006-10-26 01:34:57 kurihara Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
- *
  */
 
 #include <cppunit/ui/text/TestRunner.h>
@@ -26,45 +17,60 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/TestAssert.h>
-#include <fstream>
+
 #include "../ModuleManager.h"
 #include "../Properties.h"
 
 using namespace std;
 
-class ModuleManagerTest
+// ModuleManagerTests をテストする [3]
+class ModuleManagerTests
   : public CppUnit::TestFixture
 {
-  CPPUNIT_TEST_SUITE(ModuleManagerTest);
+  CPPUNIT_TEST_SUITE(ModuleManagerTests);
+  CPPUNIT_TEST(test_load);
+  CPPUNIT_TEST(test_unload);
+  CPPUNIT_TEST(test_unloadAll);
+  CPPUNIT_TEST(test_symbol);
+  CPPUNIT_TEST(test_setLoadpath);
+  CPPUNIT_TEST(test_getLoadPath);
+  CPPUNIT_TEST(test_addLoadpath);
+  CPPUNIT_TEST(test_getLoadedModules);
+  CPPUNIT_TEST(test_getLoadableModules);
+  CPPUNIT_TEST(test_allowAbsolutePath);
+  CPPUNIT_TEST(test_disallowAbsolutePath);
+  CPPUNIT_TEST(test_allowModuleDownload);
+  CPPUNIT_TEST(test_disallowModuleDownload);
   CPPUNIT_TEST(test_findFile);
   CPPUNIT_TEST(test_fileExist);
   CPPUNIT_TEST(test_getInitFuncName);
-  CPPUNIT_TEST(test_load);
-  CPPUNIT_TEST(test_getLoadedModules);
   CPPUNIT_TEST_SUITE_END();
+
 private:
-  
-  ModuleManager* m_pModMgr;
-  
+
+  RTC::ModuleManager* m_pModMgr;
+
 public:
   
   /*
    * コンストラクタ/デストラクタ [7]
    */
-  ModuleManagerTest()
+  ModuleManagerTests()
   {
+    
+  }
+  
+  ~ModuleManagerTests()
+  {
+  }
+  
 
-  }
-  
-  ~ModuleManagerTest()
-  {
-  }
-  
   /*
    * 初期化/後始末 [8]
    */
   virtual void setUp()
   {
+
     const char* default_properties[] = {
       "manager.modules.config_ext", "so",
       "manager.modules.config_path", "/etc/rtc",
@@ -79,121 +85,159 @@ public:
       "manager.modules.preload", "",
       ""
     };
-
-    Properties prop(default_properties);
-    m_pModMgr = new ModuleManager(prop);
+    
+    RTC::Properties prop(default_properties);
+    m_pModMgr = new RTC::ModuleManager(prop);
+    m_pModMgr->load("libRTC.so");
+    m_pModMgr->load("libm.so");
   }
   
+
   virtual void tearDown()
   { 
+    m_pModMgr->unloadAll();
     delete m_pModMgr;
   }
-  
-
-  void test_fileExist()
-  {
-    bool result;
-
-    // Success case
-    result = m_pModMgr->fileExist("/etc/hosts");
-    CPPUNIT_ASSERT_MESSAGE("fileExist failed", result);
-
-    result = m_pModMgr->fileExist("./ModuleManagerTests.cpp");
-    CPPUNIT_ASSERT_MESSAGE("fileExist failed", result);
-
-    result = m_pModMgr->fileExist("../ModuleManager.cpp");
-    CPPUNIT_ASSERT_MESSAGE("fileExist failed", result);
-
-    result = m_pModMgr->fileExist("../tests/ModuleManagerTests.cpp");
-    CPPUNIT_ASSERT_MESSAGE("fileExist failed", result);
 
 
-    // Failure case
-    result = m_pModMgr->fileExist("/etc/munya/hoge");
-    CPPUNIT_ASSERT_MESSAGE("fileExist error", !result);
+  /* tests for string load(const string& file_name) */
+  void test_load() {
+    string libname;
 
-    result = m_pModMgr->fileExist("./aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    CPPUNIT_ASSERT_MESSAGE("fileExist error", !result);
+    try {
 
-    result = m_pModMgr->fileExist("../../../../../../../../../a");
-    CPPUNIT_ASSERT_MESSAGE("fileExist error", !result);
+      // Success case
+      // ファイル名だけ与える。
+      libname = m_pModMgr->load("libRTC.so");
+      CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
 
-    result = m_pModMgr->fileExist("C:\\Program Files\\munya");
-    CPPUNIT_ASSERT_MESSAGE("fileExist error", !result);
+      libname = m_pModMgr->load("libRTC.so");
+      CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
 
-  }
+      libname = m_pModMgr->load("libRTC.so");
+      CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
 
-  void test_findFile()
-  {
-    std::string result;
-    std::vector<std::string> path;
-    path.push_back("/lib");
-    path.push_back("/usr/lib");
-    path.push_back("/usr/local/lib");
+      // ファイル名を絶対パスで与える。
+      libname = m_pModMgr->load("/usr/lib/libRTC.so");
+      CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
 
-    result = m_pModMgr->findFile("libm.so", path);
-    //    std::cout << result << std::endl;
-    CPPUNIT_ASSERT_MESSAGE("fileFile error: libm.so",
-			   result == "/usr/lib/libm.so");
-
-    result = m_pModMgr->findFile("libc.so", path);
-    CPPUNIT_ASSERT_MESSAGE("fileFile error: libc.so",
-			   result == "/usr/lib/libc.so");
-
-    result = m_pModMgr->findFile("libACE.so", path);
-    CPPUNIT_ASSERT_MESSAGE("fileFile error: libACE.so",
-			   result == "/usr/local/lib/libACE.so");
-
-    result = m_pModMgr->findFile("hosts", path);
-    CPPUNIT_ASSERT_MESSAGE("fileFile error: hosts",
-			   result == "");
-
-    result = m_pModMgr->findFile("munyamunya", path);
-    CPPUNIT_ASSERT_MESSAGE("fileFile error: munyamunya",
-			   result == "");
-
-  }
+      // ディレクトリの区切り文字に"//"や"../"がある場合。 -> OK.
+      libname = m_pModMgr->load("/usr//users/kurihara/../kurihara/Components/test/test.so");
+      CPPUNIT_ASSERT(libname == "/usr//users/kurihara/../kurihara/Components/test/test.so");
 
 
-  void test_getInitFuncName()
-  {
-    std::string result;
 
-    result = m_pModMgr->getInitFuncName("Manipulator");
-    CPPUNIT_ASSERT_MESSAGE("getInitFuncName error: Manipulator",
-			   result == "ManipulatorInit");
+      // Failure case
+      // 
+      // ファイル名だけ与える。 -> Error DLL open failed.
+      //      libname = m_pModMgr->load("libm.a");
+      //      CPPUNIT_ASSERT(libname == "/usr/lib/libm.a");
 
-    result = m_pModMgr->getInitFuncName("PHANToM");
-    CPPUNIT_ASSERT_MESSAGE("getInitFuncName error: PHANToM",
-			   result == "PHANToMInit");
+      // ディレクトリ名を与える。 -> Error DLL open failed.
+      //      libname = m_pModMgr->load("OpenRTM");
+      //      CPPUNIT_ASSERT(libname == "/usr/lib/OpenRTM");
 
-  }
-
-  //======================================================================
-  // load() tests
-  //======================================================================
-  void test_load()
-  {
-    std::vector<std::string> loadpath;
-    loadpath = m_pModMgr->getLoadPath();
-
-    for (unsigned int i = 0 ; i < loadpath.size(); ++i)
-      {
-	//	std::cout << "loadpath: " << loadpath[i] << std::endl;
-      }
-
-
-    std::string libname;
-    try
-      {
-	libname = m_pModMgr->load("libm.so");
-	//	std::cout << "test_load: " << libname << std::endl;
-      }
-    catch (ModuleManager::Error& e)
+      // 実行ファイルを与える。  -> Error Invalid file name.
+      //      libname = m_pModMgr->load("rtm-naming");
+      //      CPPUNIT_ASSERT(libname == "/usr/bin/rtm-naming");
+      
+      // 存在しないファイル名を与える。-> Error Invalid file name.
+      //      libname = m_pModMgr->load("test.test");
+      //      cout << "libname: " << libname << endl;
+      //    CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
+    }
+    catch (RTC::ModuleManager::Error& e)
       {
 	std::cout << "Error " << e.reason << std::endl;
       }
-    catch (ModuleManager::NotFound& e)
+    catch (RTC::ModuleManager::NotFound& e)
+      {
+	std::cout << "NotFound " << e.name << std::endl;
+      }
+    catch (...)
+      {
+	std::cout << "other exception" << std::endl;
+      }
+
+  }
+
+
+  /* tests for void unload(const string& file_name) */
+  void test_unload() {
+    try {
+      string libname;
+      libname = m_pModMgr->load("libRTC.so");
+      CPPUNIT_ASSERT(libname == "/usr/lib/libRTC.so");
+
+      // Success case
+      m_pModMgr->unload("/usr/lib/libRTC.so");
+      m_pModMgr->unload("/usr/lib/libm.so");
+
+
+
+      // Failure case
+      // ファイル名だけを与える。 -> NotFound. 絶対パスで与える必要がある。
+      //      m_pModMgr->unload("libRTC.so");
+
+      // loadしていないファイルをunloadする。 -> NotFound.
+      //      m_pModMgr->unload("usr/users/kurihara/Components/test/test.so");
+
+      // 一度unloadしたファイルをunloadする。 ->  NotFound.
+      //      m_pModMgr->unload("/usr/lib/libRTC.so");
+      
+    }
+    catch (RTC::ModuleManager::Error& e)
+      {
+	std::cout << "Error " << e.reason << std::endl;
+      }
+    catch (RTC::ModuleManager::NotFound& e)
+      {
+	std::cout << "NotFound " << e.name << std::endl;
+      }
+    catch (...)
+      {
+	std::cout << "other exception" << std::endl;
+      }
+  }
+
+
+  /* tests for void unloadAll() */
+  void test_unloadAll() {
+    try {
+      m_pModMgr->unloadAll();
+      m_pModMgr->unloadAll();
+      m_pModMgr->unloadAll();
+    }
+    catch (RTC::ModuleManager::Error& e)
+      {
+	std::cout << "Error " << e.reason << std::endl;
+      }
+    catch (RTC::ModuleManager::NotFound& e)
+      {
+	std::cout << "NotFound " << e.name << std::endl;
+      }
+    catch (...)
+      {
+	std::cout << "other exception" << std::endl;
+      }
+  }
+
+
+  /* tests for void* symbol(const string& file_name, const string& func_name) */
+  void test_symbol() {
+
+    //============ 安藤氏作成のテストプログラムより抜粋 ==============================
+    string libname;
+
+    try
+      {
+	libname = m_pModMgr->load("libm.so");
+      }
+    catch (RTC::ModuleManager::Error& e)
+      {
+	std::cout << "Error " << e.reason << std::endl;
+      }
+    catch (RTC::ModuleManager::NotFound& e)
       {
 	std::cout << "NotFound " << e.name << std::endl;
       }
@@ -222,68 +266,229 @@ public:
     CPPUNIT_ASSERT_MESSAGE("load error: sin(0.0)",
 			   (*_sin)(0.0) == 0.0);
     CPPUNIT_ASSERT_MESSAGE("load error: sin(pi/2)",
-			   (*_sin)(3.141592653589793238462643383279/2) == 1.0);
+			   (*_sin)(3.141592653589793238462643383279/2) == 1.0);   
 
-    m_pModMgr->unload(libname);
+    //===============================================================================
+
+    libname = m_pModMgr->load("/usr/users/kurihara/RTM/Components/test/test.so");
+    typedef void (*testHello)();
+
+    testHello _th;
+    // nm --dynamic test.soで"hello"が含まれる文字列(シンボル)を探して使用した。
+    _th = (testHello) m_pModMgr->symbol(libname, "_ZN4test5helloEv");
+    (*_th)();
   }
 
-  void test_getLoadedModules()
-  {
-    std::string name;
-    name = m_pModMgr->load("/usr/lib/libm.so");
-    //    std::cout << "load(): " << name << std::endl;
-    name = m_pModMgr->load("libc.so");
-    //    std::cout << "load(): " << name << std::endl;
-    name = m_pModMgr->load("/usr/local/lib/libACE.so");
-    //    std::cout << "load(): " << name << std::endl;
-    name = m_pModMgr->load("libboost_regex.so");
-    //    std::cout << "load(): " << name << std::endl;
-    name = m_pModMgr->load("/usr/local/lib/libomniORB4.so");
-    //    std::cout << "load(): " << name << std::endl;
 
-    std::vector<std::string> module;
+  /* tests for void setLoadpath(const vector<string>& load_path) */
+  void test_setLoadpath() {
+    vector<string> set_loadpath, get_loadpath;
+    set_loadpath.push_back("/usr");
+    set_loadpath.push_back("/usr/lib");
+    set_loadpath.push_back("/usr/local/lib");
+    set_loadpath.push_back("/tmp");
+    set_loadpath.push_back("/usr/users/aaaaaaaaaaa/bbbbbbbbbbbb/ccccccccccccc/ddddddddddddd/eeeeeeeeeeeee/ffffffffffffffff/ggggggggggggg/hhhhhhhhhhhhh/iiiiiiiiiii");
+
+    m_pModMgr->setLoadpath(set_loadpath);
+    get_loadpath = m_pModMgr->getLoadPath();
+
+    unsigned int size = get_loadpath.size();
+
+    for (unsigned int i = 0; i < size; i++) {
+      CPPUNIT_ASSERT(set_loadpath[i] == get_loadpath[i]);
+    }
+  }
+
+
+  /* tests for vector<string> getLoadPath() */
+  void test_getLoadPath() {
+    vector<string> getlist, expectation;
+    expectation.push_back("/usr/lib");
+    expectation.push_back("/usr/local/lib");
+    expectation.push_back("/usr/local/lib/rtc");
+    getlist = m_pModMgr->getLoadPath();
     
-    module = m_pModMgr->getLoadedModules();
+    for (unsigned int i = 0; i < getlist.size(); i++) {
+      CPPUNIT_ASSERT(getlist[i] == expectation[i]);
+    }
+  }
 
-    for (unsigned int i = 0; i < module.size(); i++)
+
+  /* tests for void addLoadpath(const vector<string>& load_path) */
+  void test_addLoadpath() {
+    vector<string> getlist, expectation, add_path;
+    expectation.push_back("/usr/lib");
+    expectation.push_back("/usr/local/lib");
+    expectation.push_back("/usr/local/lib/rtc");
+    expectation.push_back("/tmp");
+    expectation.push_back("/hoge");
+    expectation.push_back("/hoge/hoge");
+
+    add_path.push_back("/tmp");
+    add_path.push_back("/hoge");
+    add_path.push_back("/hoge/hoge");
+    m_pModMgr->addLoadpath(add_path);
+
+    getlist = m_pModMgr->getLoadPath();
+    for (unsigned int i = 0; i < getlist.size(); i++) {
+      CPPUNIT_ASSERT(getlist[i] == expectation[i]);
+    }
+  }
+
+
+  /* tests for vector<string> getLoadedModules() */
+  void test_getLoadedModules() {
+    vector<string> get_modlist;
+
+    get_modlist = m_pModMgr->getLoadedModules();
+    CPPUNIT_ASSERT(get_modlist[0] == "/usr/lib/libRTC.so");
+    CPPUNIT_ASSERT(get_modlist[1] == "/usr/lib/libm.so");
+  }
+
+
+  /* tests for vector<string> getLoadableModules() */
+  void test_getLoadableModules() {
+    // ModuelManager.cppで実装されていないl。
+
+    vector<string> get_modlist;
+    get_modlist = m_pModMgr->getLoadableModules();
+    for (unsigned int i = 0; i < get_modlist.size(); i++) {
+      cout << get_modlist[i] << endl;
+    }
+    
+  }
+
+
+  /* tests for void allowAbsolutePath() */
+  void test_allowAbsolutePath() {
+    string libname;
+    try {
+
+      // Success case
+      m_pModMgr->allowAbsolutePath();
+      libname = m_pModMgr->load("libRTC.so");
+      m_pModMgr->unload(libname);
+
+      libname = m_pModMgr->load("/usr/lib/libm.so");
+      m_pModMgr->unload(libname);
+
+      libname = m_pModMgr->load("../lib/libm.so");
+      m_pModMgr->unload(libname);
+      
+
+      m_pModMgr->disallowAbsolutePath();
+      libname = m_pModMgr->load("../lib/libRTC.so");
+      m_pModMgr->unload(libname);
+
+      // Failure case
+      //      libname = m_pModMgr->load("/usr/lib/libRTC.so");
+      //      m_pModMgr->unload(libname);
+    }
+    catch (RTC::ModuleManager::Error& e)
       {
-	//	std::cout << "test_getLoadedModules(): " << module[i] << std::endl;
+	std::cout << "Error " << e.reason << std::endl;
       }
-#define FOUND(path) find(module.begin(), module.end(), path) != module.end()
-
-    CPPUNIT_ASSERT_MESSAGE("getLoadedModules error: /usr/lib/libm.so",
-			   FOUND("/usr/lib/libm.so"));
-
-    CPPUNIT_ASSERT_MESSAGE("getLoadedModules error: /usr/lib/libc.so",
-			   FOUND("/usr/lib/libc.so"));
-
-    CPPUNIT_ASSERT_MESSAGE("getLoadedModules error: /usr/local/lib/libACE.so",
-			   FOUND("/usr/local/lib/libACE.so"));
-
-    CPPUNIT_ASSERT_MESSAGE("getLoadedModules error: /usr/local/lib/libboost_regex.so",
-			   FOUND("/usr/local/lib/libboost_regex.so"));
-
-    CPPUNIT_ASSERT_MESSAGE("getLoadedModules error: /usr/local/lib/libomniORB4.so",
-			   FOUND("/usr/local/lib/libomniORB4.so"));
+    catch (RTC::ModuleManager::NotFound& e)
+      {
+	std::cout << "NotFound " << e.name << std::endl;
+      }
+    catch (...)
+      {
+	std::cout << "other exception" << std::endl;
+      }
+  }
 
 
-    // UnloadAll() test
-    m_pModMgr->unloadAll();
+  /* tests for void disallowAbsolutePath() */
+  void test_disallowAbsolutePath() {
+    // test_allowAbsolutePath()にてテスト済み。
+  }
 
-    module = m_pModMgr->getLoadedModules();
 
-    // list of loaded modules should be size 0;
-    CPPUNIT_ASSERT_MESSAGE("unloadAll error:",
-			   module.size() == 0);
+  /* tests for void allowModuleDownload() */
+  void test_allowModuleDownload() {
+    // テストまだ
+  }
+
+
+  /* tests for void disallowModuleDownload() */
+  void test_disallowModuleDownload() {
+    // テストまだ
+  }
+
+
+  /* tests for string findFile(const string& fname, const vector<string>& load_path) */
+  void test_findFile() {
+    //============ 安藤氏作成のテストプログラムより抜粋 ==============================
+    std::string result;
+    std::vector<std::string> path;
+    path.push_back("/lib");
+    path.push_back("/usr/lib");
+    path.push_back("/usr/local/lib");
+
+    result = m_pModMgr->findFile("libm.so", path);
+    CPPUNIT_ASSERT_MESSAGE("fileFile error: libm.so",
+			   result == "/usr/lib/libm.so");
+
+    result = m_pModMgr->findFile("libc.so", path);
+    CPPUNIT_ASSERT_MESSAGE("fileFile error: libc.so",
+			   result == "/usr/lib/libc.so");
+
+    result = m_pModMgr->findFile("libACE.so", path);
+    CPPUNIT_ASSERT_MESSAGE("fileFile error: libACE.so",
+			   result == "/usr/lib/libACE.so");
+
+    result = m_pModMgr->findFile("hosts", path);
+    CPPUNIT_ASSERT_MESSAGE("fileFile error: hosts",
+			   result == "");
+
+    result = m_pModMgr->findFile("munyamunya", path);
+    CPPUNIT_ASSERT_MESSAGE("fileFile error: munyamunya",
+			   result == "");
+   
+    //================================================================================
+
+    // Failure case
+    //    path.clear();
+    //    result = m_pModMgr->findFile("libm.so", path);
+    //    CPPUNIT_ASSERT_MESSAGE("fileFile error: libm.so",
+    //    			   result == "/usr/lib/libm.so");
 
   }
 
+
+  /* tests for bool fileExist(const string& filename) */
+  void test_fileExist() {
+    // Success case
+    CPPUNIT_ASSERT(m_pModMgr->fileExist("../../../../../../../../usr/lib/libm.so"));
+    CPPUNIT_ASSERT(m_pModMgr->fileExist("/usr/lib/libm.so"));
+
+    // Failure case
+    //    CPPUNIT_ASSERT(m_pModMgr->fileExist("libm.so"));
+  }
+
+
+  /* tests for string getInitFuncName(const string& file_path) */
+  void test_getInitFuncName() {
+    std::string result;
+
+    //============ 安藤氏作成のテストプログラムより抜粋 ==============================
+    result = m_pModMgr->getInitFuncName("Manipulator");
+    CPPUNIT_ASSERT_MESSAGE("getInitFuncName error: Manipulator",
+			   result == "ManipulatorInit");
+
+    result = m_pModMgr->getInitFuncName("PHANToM");
+    CPPUNIT_ASSERT_MESSAGE("getInitFuncName error: PHANToM",
+			   result == "PHANToMInit");
+    //================================================================================
+  }
 };
+
+
 /*
  * register test suite
  */
-CPPUNIT_TEST_SUITE_REGISTRATION(ModuleManagerTest);
-
+CPPUNIT_TEST_SUITE_REGISTRATION(ModuleManagerTests);
 
 
 
