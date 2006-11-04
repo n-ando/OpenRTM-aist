e07ee631e006f3305fdcc0c99069420bdd5efc84
n-ando <Noriaki.Ando@gmail.com>
2006-11-04 16:34:47 +0000

    Some trivial fixes.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@170 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- build/makewrapper.py
+++ build/makewrapper.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 #
 # @brief CORBA stub and skelton wrapper generator
-# @date $Date: 2006-09-11 18:33:14 $
+# @date $Date: 2006-11-04 16:34:47 $
 # @author Norkai Ando <n-ando@aist.go.jp>
 #
-# Copyright (C) 2005
+# Copyright (C) 2005-2006
+#     Noriaki Ando
 #     Task-intelligence Research Group,
 #     Intelligent Systems Research Institute,
 #     National Institute of
 #         Advanced Industrial Science and Technology (AIST), Japan
 #     All rights reserved.
 #
-# $Id: makewrapper.py,v 1.3 2006-09-11 18:33:14 n-ando Exp $
+# $Id: makewrapper.py,v 1.4 2006-11-04 16:34:47 n-ando Exp $
 #
 
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2006/09/11 18:33:14  n-ando
+# A prefix of include directory can be specified with command options.
+#
 # Revision 1.2  2006/03/31 05:26:34  n-ando
 # - Almost all functions are rewritten to use ezt.py (Easy Template module).
 # - Now makewrapper.py uses ezt.py (Easy Template module).
@@ -156,69 +160,70 @@ stub_h_temp = """// -*- C++ -*-
 """
 
 class wrapper_data:
-	def __init__(self, basename, dir_name):
-		self.data = {}
-		self.data["basename"] = basename
-		self.data["BASENAME"] = basename.upper()
-		self.data["idl_name"] = basename + ".idl"
-		self.data["skel_dir"] = dir_name
-		self.data["date"]     = time.ctime()
-		self.data["skel_cpp"] = basename + "Skel.cpp"
-		self.data["skel_h"]   = basename + "Skel.h"
-		self.data["stub_cpp"] = basename + "Stub.cpp"
-		self.data["stub_h"]   = basename + "Stub.h"
-
-	def get_dict(self):
-		return self.data
+    def __init__(self, basename, dir_name):
+        self.data = {}
+        self.data["basename"] = basename
+        self.data["BASENAME"] = basename.upper()
+        self.data["idl_name"] = basename + ".idl"
+        self.data["skel_dir"] = dir_name
+        self.data["date"]     = time.ctime()
+        self.data["skel_cpp"] = basename + "Skel.cpp"
+        self.data["skel_h"]   = basename + "Skel.h"
+        self.data["stub_cpp"] = basename + "Stub.cpp"
+        self.data["stub_h"]   = basename + "Stub.h"
+        
+    def get_dict(self):
+        return self.data
 		
 
 class wrapper_gen:
-	def __init__(self, data):
-		self.data = data
+    def __init__(self, data):
+        self.data = data
 
-	def gen(self, fname, temp_txt, data):
-		f = file(fname, "w")
-		#		s = StringIO.StringIO()
-		t = ezt.Template(compress_whitespace = 0)
-		t.parse(temp_txt)
-		t.generate(f, data)
-		#		gen_txt = s.getvalue().splitlines()
-		#		f.write(gen_txt)
-		f.close()
-		print "\"", fname, "\"" " was generated."
-		return
+    def gen(self, fname, temp_txt, data):
+        f = file(fname, "w")
+        #		s = StringIO.StringIO()
+        t = ezt.Template(compress_whitespace = 0)
+        t.parse(temp_txt)
+        t.generate(f, data)
+        #		gen_txt = s.getvalue().splitlines()
+        #		f.write(gen_txt)
+        f.close()
+        print "\"", fname, "\"" " was generated."
+        return
 
-	def gen_all(self):
-		self.write_skel()
-		self.write_skelh()
-		self.write_stub()
-		self.write_stubh()
-		return
+    def gen_all(self):
+        self.write_skel()
+        self.write_skelh()
+        self.write_stub()
+        self.write_stubh()
+        return
 
-	def write_skel(self):
-		self.gen(self.data["skel_cpp"], skel_cpp_temp, self.data)
+    def write_skel(self):
+        self.gen(self.data["skel_cpp"], skel_cpp_temp, self.data)
+        return
 
-	def write_skelh(self):
-		self.gen(self.data["skel_h"], skel_h_temp, self.data)
-		return
+    def write_skelh(self):
+        self.gen(self.data["skel_h"], skel_h_temp, self.data)
+        return
 
-	def write_stub(self):
-		self.gen(self.data["stub_cpp"], stub_cpp_temp, self.data)
-		return
+    def write_stub(self):
+        self.gen(self.data["stub_cpp"], stub_cpp_temp, self.data)
+        return
 
-	def write_stubh(self):
-		self.gen(self.data["stub_h"], stub_h_temp, self.data)
-		return
+    def write_stubh(self):
+        self.gen(self.data["stub_h"], stub_h_temp, self.data)
+        return
 
 
 idl_file = sys.argv[1]
 if len(sys.argv) > 2:
-	skel_dir = sys.argv[2]
+    skel_dir = sys.argv[2]
 else:
-	skel_dir = "rtm/idl"
+    skel_dir = "rtm/idl"
 
 basename = re.sub(".idl", "", idl_file);
-
+    
 data = wrapper_data(basename, skel_dir)
 gen  = wrapper_gen(data.get_dict())
 gen.gen_all()
