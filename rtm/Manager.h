1bb4bb20a76295f0a32a19828770381bce8337c5
n-ando <Noriaki.Ando@gmail.com>
2006-11-06 01:31:57 +0000

    Some Manager's functions has been implemented. - Component creation process - System logger initialization
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@186 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/Manager.h
+++ rtm/Manager.h
@@ -2,7 +2,7 @@
 /*!
  * @file Manager.h
  * @brief RTComponent manager class
- * @date $Date: 2006-10-25 17:28:05 $
+ * @date $Date: 2006-11-06 01:31:57 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -12,12 +12,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: Manager.h,v 1.2 2006-10-25 17:28:05 n-ando Exp $
+ * $Id: Manager.h,v 1.3 2006-11-06 01:31:57 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/10/25 17:28:05  n-ando
+ * Component factory registration and relative functions are implemented.
+ *
  * Revision 1.1  2006/10/17 10:21:24  n-ando
  * The first commitment.
  *
@@ -35,28 +38,23 @@
 
 #include <rtm/Factory.h>
 #include <rtm/ObjectManager.h>
+#include <rtm/RTObject.h>
+#include <rtm/SystemLogger.h>
 
 class Properties;
-namespace RTC
-{
-  class RtcBase
-  {
-  public:
-    const char* instance_id();
-  };
-};
-
 
 namespace RTC
 {
-
+  class CorbaNaming;
   class ModuleManager;
+  class NamingManager;
+  class CorbaObjectManager;
 
   class Manager
   {
   protected:
-    Manager(){};
-    Manager(const Manager& manager){};
+    Manager();
+    Manager(const Manager& manager);
       //      Manager& operator=(const Manager& manager){return manager;};
     
   public:
@@ -121,11 +119,106 @@ namespace RTC
 
 
     //============================================================
+    // Manager
+    //============================================================
+    
+    /*!
+     * @if jp
+     *
+     * @brief Managerのアクティブ化
+     *
+     * このオペレーションは以下の処理を行う
+     * - CORBA POAManager のアクティブ化
+     * - マネージャCORBAオブジェクトのアクティブ化
+     * - Manager オブジェクトへのオブジェクト参照の登録
+     *
+     * このオペレーションは、マネージャの初期化後、runManager()
+     * の前に呼ぶ必要がある。
+     *
+     * @else
+     *
+     * @brief Activate Manager
+     *
+     * This operation do the following,
+     * - Activate CORBA POAManager
+     * - Activate Manager CORBA object
+     * - Bind object reference of the Manager to the nameserver
+     *
+     * This operationo should be invoked after Manager:init(),
+     * and before tunManager().
+     *
+     * @endif
+     */
+    bool activateManager();
+
+    /*!
+     * @if jp
+     *
+     * @brief Managerの実行
+     *
+     * このオペレーションはマネージャのメインループを実行する。
+     * このメインループ内では、CORBA ORBのイベントループ等が
+     * 処理される。デフォルトでは、このオペレーションはブロックし、
+     * Manager::destroy() が呼ばれるまで処理を戻さない。
+     * 引数 no_block が true に設定されている場合は、内部でイベントループ
+     * を処理するスレッドを起動し、ブロックせずに処理を戻す。
+     *
+     * @param no_block false: ブロッキングモード, true: ノンブロッキングモード
+     *
+     * @else
+     *
+     * @brief Run the Manager
+     *
+     * This operation processes the main event loop of the Manager.
+     * In this main loop, CORBA's ORB event loop or other processes
+     * are performed. As the default behavior, this operation is going to
+     * blocking mode and never returns until manager::destroy() is called.
+     * When the given argument "no_block" is set to "true", this operation
+     * creates a thread to process the event loop internally, and it doesn't
+     * block and returns.
+     *
+     * @param no_block false: Blocking mode, true: non-blocking mode.
+     *
+     * @endif
+     */
+    void runManager(bool no_block = false);
+    
+    typedef void (*InitProc)(Manager* manager);
+
+    /*!
+     * @if jp
+     *
+     * @brief 初期化プロシージャのセット
+     *
+     * このオペレーションはユーザが行うモジュール等の初期化プロシージャ
+     * を設定する。ここで設定されたプロシージャは、マネージャが初期化され、
+     * アクティブ化された後、適切なタイミングで実行される。
+     *
+     * @param proc 初期化プロシージャの関数ポインタ
+     *
+     * @else
+     *
+     * @brief Run the Manager
+     *
+     * This operation sets the initial procedure call to process module
+     * initialization, other user defined initialization and so on.
+     * The given procedure will be called at the proper timing after the 
+     * manager initialization, activation and run.
+     *
+     * @param proc A function pointer to the initial procedure call
+     *
+     * @endif
+     */
+    void setInitProc(InitProc proc);
+
+
+
+    //============================================================
     // Module management
     //============================================================
     /*!
      * @if jp
-     * @brief モジュールのロード
+     * @brief [CORBA interface] モジュールのロード
      *
      * コンポーネントのモジュールをロードして初期化関数を実行する。
      *
@@ -133,7 +226,8 @@ namespace RTC
      * @param initfunc 初期化関数名
      * 
      * @else
-     * @brief Load module
+     *
+     * @brief [CORBA interface] Load module
      *
      * Load module (shared library, DLL etc..) by file name,
      * and invoke initialize function.
@@ -185,7 +279,7 @@ namespace RTC
      * @endif
      */ 
     void unloadAll();
-    
+
 
     /*!
      * @if jp
@@ -220,41 +314,253 @@ namespace RTC
 			 RtcNewFunc new_func,
 			 RtcDeleteFunc delete_func);
     
+    /*!
+     * @if jp
+     * @brief ファクトリ全リストを取得する
+     * @else
+     * @brief Get the list of all RT-Component Factory
+     * @endif
+     */
     std::vector<std::string> getModulesFactories();
     
     //============================================================
     // Component management
     //============================================================
+    /*!
+     * @if jp
+     * @brief RTコンポーネントを生成する
+     * @else
+     * @brief Create RT-Component
+     * @endif
+     */
     RtcBase* createComponent(const char* module_name);
 
+    /*!
+     * @if jp
+     * @brief RTコンポーネントを直接 Manager に登録する
+     * @else
+     * @brief Register RT-Component directly without Factory
+     * @endif
+     */
     bool registerComponent(RtcBase* comp);
-    
+
+    /*!
+     * @if jp
+     * @brief Manager に登録されているRTコンポーネントを削除する
+     * @else
+     * @brief Unregister RT-Component that is registered in the Manager
+     * @endif
+     */
     void deleteComponent(const char* instance_name);
     
-
+    /*!
+     * @if jp
+     * @brief Manager に登録されているRTコンポーネントを取得する
+     * @else
+     * @brief Get RT-Component's pointer
+     * @endif
+     */
     RtcBase* getComponent(const char* instance_name);
     
+    /*!
+     * @if jp
+     * @brief Manager に登録されている全RTコンポーネントを取得する
+     * @else
+     * @brief Get all RT-Component's pointer
+     * @endif
+     */
     std::vector<RtcBase*> getComponents();
     
 
+    //============================================================
+    // CORBA 関連
+    //============================================================
+    /*!
+     * @if jp
+     * @brief ORB のポインタを取得する
+     * @else
+     * @brief Get the pointer to the ORB
+     * @endif
+     */
+    CORBA::ORB_ptr getORB();
+
+    /*!
+     * @if jp
+     * @brief Manager が持つ RootPOA のポインタを取得する
+     * @else
+     * @brief Get the pointer to the RootPOA 
+     * @endif
+     */
+    PortableServer::POA_ptr getPOA();
+    PortableServer::POAManager_ptr getPOAManager();
+
+
   protected:
-    void _init(int argc, char** argv);
+    /*!
+     * @if jp
+     * @brief Manager の内部初期化処理
+     * @else
+     * @brief Manager internal initialization
+     * @endif
+     */
+    void initManager(int argc, char** argv);
+
+
+    /*!
+     * @if jp
+     * @brief CORBA ORB の初期化処理
+     * @else
+     * @brief CORBA ORB initialization
+     * @endif
+     */
+    bool initORB();
+
+
+    /*!
+     * @if jp
+     * @brief NamingManager の初期化
+     * @else
+     * @brief NamingManager initialization
+     * @endif
+     */
+    bool initNaming();
+
+
+    /*!
+     * @if jp
+     * @brief System logger の初期化
+     * @else
+     * @brief System logger initialization
+     * @endif
+     */
+    bool initLogger();
+
+
+
+    bool mergeProperty(Properties& prop, const char* file_name);
+    std::string formatString(const char* naming_format,
+			     Properties& prop);
     
+    //============================================================
+    // protected 変数
+    //============================================================
+    /*!
+     * @if jp
+     * @brief 唯一の Manager へのポインタ
+     * @else
+     * @brief The pointer to the Manager
+     * @endif
+     */
     static Manager* manager;
+
+    /*!
+     * @if jp
+     * @brief 唯一の Manager へのポインタに対する mutex
+     * @else
+     * @brief The mutex of the pointer to the Manager 
+     * @endif
+     */
     static ACE_Thread_Mutex mutex;
     
-    
-    
+    /*!
+     * @if jp
+     * @brief ORB へのポインタ
+     * @else
+     * @brief The pointer to the ORB
+     * @endif
+     */
+    CORBA::ORB_var m_pORB;
+
+    /*!
+     * @if jp
+     * @brief POA へのポインタ
+     * @else
+     * @brief The pointer to the POA
+     * @endif
+     */
+    PortableServer::POA_var m_pPOA;
+
+    /*!
+     * @if jp
+     * @brief POAManager へのポインタ
+     * @else
+     * @brief The pointer to the POAManager
+     * @endif
+     */
+    PortableServer::POAManager_var m_pPOAManager;
+
+
+    /*!
+     * @if jp
+     * @brief Propery へのポインタ
+     * @else
+     * @brief The pointer to the Property
+     * @endif
+     */
     Properties* m_config;
+
+    /*!
+     * @if jp
+     * @brief ModuleManager へのポインタ
+     * @else
+     * @brief The pointer to the ModuleManager
+     * @endif
+     */
     ModuleManager* m_module;
     
+    /*!
+     * @if jp
+     * @brief ModuleManager へのポインタ
+     * @else
+     * @brief The pointer to the ModuleManager
+     * @endif
+     */
+    NamingManager* m_namingManager;
+
+    CorbaObjectManager* m_objManager;
+
+
+    InitProc m_initProc;
+
+    /*!
+     * @if jp
+     * @brief ロガーバッファ
+     * @else
+     * @brief Logger buffer
+     * @endif
+     */
+    Logbuf m_Logbuf;
+
+    /*!
+     * @if jp
+     * @brief ロガー仲介バッファ
+     * @else
+     * @brief Logger mediation buffer
+     * @endif
+     */
+    MedLogbuf m_MedLogbuf;
+
+    /*!
+     * @if jp
+     * @brief ロガーストリーム
+     * @else
+     * @brief Logger stream
+     * @endif
+     */
+    LogStream rtcout;
+    
+
+    //============================================================
+    // コンポーネントマネージャ
+    //============================================================
+    // ObjectManager へ渡す述語クラス
     struct InstanceName
     {
-      InstanceName(RtcBase* comp) : m_name(comp->instance_id()) {};
+      InstanceName(RtcBase* comp) : m_name(comp->getInstanceName()) {};
       InstanceName(const char* name) : m_name(name) {};
       bool operator()(RtcBase* comp)
       {
-	return m_name == comp->instance_id();
+	return m_name == comp->getInstanceName();
       }
       std::string m_name;
     };
@@ -262,35 +568,61 @@ namespace RTC
     typedef ObjectManager<std::string,
 			  RtcBase,
 			  InstanceName> ComponentManager;
-    ComponentManager m_component;
+    /*!
+     * @if jp
+     * @brief コンポーネントマネージャ
+     * @else
+     * @brief The ComponentManager
+     * @endif
+     */
+    ComponentManager m_compManager;
 
+
+    //============================================================
+    // コンポーネントファクトリ
+    //============================================================
+    // コンポーネントファクトリへ渡す述語クラス
     struct FactoryPredicate
     {
       FactoryPredicate(const char* name) : m_name(name){};
       FactoryPredicate(FactoryBase* factory)
-	: m_name(factory->profile().getProperty("name")) {};
+	: m_name(factory->profile().getProperty("implementation_id")) {};
       bool operator()(FactoryBase* factory)
       {
-	return m_name == factory->profile().getProperty("name");
+	return m_name == factory->profile().getProperty("implementation_id");
       }
       std::string m_name;
     };
-      
+    /*!
+     * @if jp
+     * @brief コンポーネントファクトリ
+     * @else
+     * @brief The ComponentFactory
+     * @endif
+     */
     typedef ObjectManager<const char*,
 			  FactoryBase,
 			  FactoryPredicate> FactoryManager;
+    /*!
+     * @if jp
+     * @brief ComponentManager
+     * @else
+     * @brief The ComponentManager
+     * @endif
+     */
     FactoryManager m_factory;
 
+    // ファクトリ名をリストアップするためのファンクタ
     struct ModuleFactories
     {
       void operator()(FactoryBase* f)
       {
-	modlist.push_back(f->profile().getProperty("name"));
+	modlist.push_back(f->profile().getProperty("implementation_id"));
       }
       std::vector<std::string> modlist;
     };
 
-  };
-};
+  }; // class Manager
+}; // namespace RTC
 
 #endif // Manager_h
