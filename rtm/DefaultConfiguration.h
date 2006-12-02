3922b402043c7fb467117c4dacec99e4621592a9
n-ando <Noriaki.Ando@gmail.com>
2006-12-02 18:37:06 +0000

    Default configuration parameters were changed.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@250 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/DefaultConfiguration.h
+++ rtm/DefaultConfiguration.h
@@ -2,7 +2,7 @@
 /*!
  * @file DefaultConfiguration.h
  * @brief RTC manager default configuration
- * @date $Date: 2006-10-23 08:40:02 $
+ * @date $Date: 2006-12-02 18:37:06 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -12,40 +12,47 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: DefaultConfiguration.h,v 1.2 2006-10-23 08:40:02 n-ando Exp $
+ * $Id: DefaultConfiguration.h,v 1.3 2006-12-02 18:37:06 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/10/23 08:40:02  n-ando
+ * The last entry "" that inidicate the end of entry did not exist. Fixed it.
+ *
  * Revision 1.1  2006/10/17 19:30:45  n-ando
  * The first comitment.
  *
  *
  */
 
+#include "rtm/version.h"
+
 static const char* default_config[] =
   {
-    "config.version",      "0.4.0",
-    "manager.os.name",     "",
-    "manager.os.release",  "",
-    "manager.os.version",  "",
-    "manager.os.arch",     "",
-    "manager.os.hostname", "",
+    "config.version",      openrtm_version,
+    "openrtm.version",     openrtm_name,
+    "manager.name",        "manager",
     "manager.pid",         "",
-    "manager.logger.file", "",
-    "manager.logger.path", "",
-    "manager.logger.master_logger", "",
-    "manager.logger.master_logger_name", "",
-    "manager.logger.time_format", "",
-    "manager.module.conf_path", "",
-    "manager.module.load_path", "",
+    "os.name",             "",
+    "os.release",          "",
+    "os.version",          "",
+    "os.arch",             "",
+    "os.hostname",         "",
+    "logger.file_name",    "./rtc%p.log",
+    "logger.date_format",  "%b %d %H:%M:%S",
+    "logger.log_level",    "NORMAL",
+    "logger.stream_lock",  "disable",
+    "logger.master_logger","",
+    "module.conf_path",    "",
+    "module.load_path",    "",
     "manager.name_service", "CORBA",
     "menager.name_server", "localhost",
     "manager.name_format", "%H/%M/%m",
-    "manager.orb.args", "",
-    "manager.orb.addr_port", "",
-    "manager.orb.id", "",
+    "orb.args",            "",
+    "orb.addr_port",       "",
+    "orb.id",              corba_name,
     ""
   };
 
