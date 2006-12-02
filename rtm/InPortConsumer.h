e7ca86bc5fb7a0f2a67064aab146263c3d9da2b3
n-ando <Noriaki.Ando@gmail.com>
2006-12-02 18:39:06 +0000

    InPortCorbaConsumer class moved to InPortCorbaConsumer.h
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@251 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/InPortConsumer.h
+++ rtm/InPortConsumer.h
@@ -2,7 +2,7 @@
 /*!
  * @file  InPortConsumer.h
  * @brief InPortConsumer class
- * @date  $Date: 2006-11-27 09:44:39 $
+ * @date  $Date: 2006-12-02 18:39:06 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,20 +13,19 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: InPortConsumer.h,v 1.1 2006-11-27 09:44:39 n-ando Exp $
+ * $Id: InPortConsumer.h,v 1.2 2006-12-02 18:39:06 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/11/27 09:44:39  n-ando
+ * The first commitment.
+ *
  */
 
 #ifndef InPortConsumer_h
 #define InPortConsumer_h
-
-#include <rtm/idl/DataPortSkel.h>
-#include <rtm/BufferBase.h>
-#include <rtm/CorbaConsumer.h>
 
 namespace RTC
 {
@@ -67,38 +66,6 @@ namespace RTC
   
   private:
 
-  };
-
-
-  template <class DataType>
-  class InPortCorbaConsumer
-    : public InPortConsumer,
-      public Consumer<RTC::OutPortAny>
-  {
-  public:
-    InPortCorbaConsumer(BufferBase<DataType>* buffer)
-      : m_buffer(buffer)
-    {}
-    virtual ~InPortCorbaConsumer()
-    {}
-
-    void put(DataType& data)
-    {
-      CORBA::Any* tmp(new CORBA::Any());
-      tmp <<= data;
-      m_inport->put(tmp);
-    }
-
-    void push()
-    {
-      CORBA::Any* tmp(new CORBA::Any());
-      tmp <<= m_buffer->get();
-      m_inport->put(tmp);
-    }
-
-  private:
-    RTC::InPortAny_var m_inport;
-    BufferBase<DataType>* m_buffer;
   };
 };     // namespace RTC
 #endif // InPortConsumer_h
