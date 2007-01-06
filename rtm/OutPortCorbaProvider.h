4244a0d15cf12e76cdac634955911f262791db3b
n-ando <Noriaki.Ando@gmail.com>
2007-01-06 17:57:35 +0000

    Interface subscription/unsubscription functions (subscribeInterface() and unsubscribeInterface()) are added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@279 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/OutPortCorbaProvider.h
+++ rtm/OutPortCorbaProvider.h
@@ -2,7 +2,7 @@
 /*!
  * @file  OutPortCorbaProvider.h
  * @brief OutPortCorbaProvider class
- * @date  $Date: 2006-12-02 18:48:21 $
+ * @date  $Date: 2007-01-06 17:57:35 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: OutPortCorbaProvider.h,v 1.1 2006-12-02 18:48:21 n-ando Exp $
+ * $Id: OutPortCorbaProvider.h,v 1.2 2007-01-06 17:57:35 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/12/02 18:48:21  n-ando
+ * The first commitment.
+ *
  * Revision 1.1  2006/11/27 09:44:42  n-ando
  * The first commitment.
  *
@@ -29,6 +32,7 @@
 
 #include <rtm/idl/DataPortSkel.h>
 #include <rtm/BufferBase.h>
+#include <rtm/OutPortProvider.h>
 
 namespace RTC
 {
@@ -43,9 +47,11 @@ namespace RTC
    */
   template <class DataType>
   class OutPortCorbaProvider
-    : public virtual POA_RTC::OutPortAny,
+    : public OutPortProvider,
+      public virtual POA_RTC::OutPortAny,
       public virtual PortableServer::RefCountServantBase
   {
+  public:
     /*!
      * @if jp
      * @brief コンストラクタ
@@ -53,9 +59,24 @@ namespace RTC
      * @brief Constructor
      * @endif
      */
-    OutPortCorbaProvider(BufferBase<DataType>* buffer)
+    OutPortCorbaProvider(BufferBase<DataType>& buffer)
       : m_buffer(buffer)
-    {}
+    {
+      CORBA::Any any_var;
+      DataType   tmp_var;
+      any_var <<= tmp_var;
+      
+      // PortProfile setting
+      setDataType(any_var.type()->name());
+      setInterfaceType("CORBA_Any");
+      setDataFlowType("Push, Pull");
+      setSubscriptionType("Any");
+
+      // ConnectorProfile setting
+      CORBA_SeqUtil::push_back(m_properties,
+			       NVUtil::newNV("dataport.corba_any.outport_ref",
+					     this->_this()));
+    }
     
     /*!
      * @if jp
@@ -69,13 +90,16 @@ namespace RTC
 
     CORBA::Any* get()
     {
+      DataType data;
       CORBA::Any_var tmp = new CORBA::Any();
-      tmp <<= m_buffer->read();
+      m_buffer.read(data);
+      tmp <<= data;
       return tmp._retn();
     }
-    
+
+   
   private:
-    BufferBase<DataType>* m_buffer;
+    BufferBase<DataType>& m_buffer;
   };
 };     // namespace RTC
 #endif // OutPortCorbaProvider_h
