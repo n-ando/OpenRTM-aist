1862d00a8d70b40a8875fcabaaf767af3796961c
n-ando <Noriaki.Ando@gmail.com>
2007-01-06 17:50:57 +0000

    Interface subscription/unsubscription functions (subscribeInterface() and unsubscribeInterface()) are added. The clone() function to clone the instance is added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@275 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/InPortConsumer.h
+++ rtm/InPortConsumer.h
@@ -2,7 +2,7 @@
 /*!
  * @file  InPortConsumer.h
  * @brief InPortConsumer class
- * @date  $Date: 2006-12-02 18:39:06 $
+ * @date  $Date: 2007-01-06 17:50:22 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: InPortConsumer.h,v 1.2 2006-12-02 18:39:06 n-ando Exp $
+ * $Id: InPortConsumer.h,v 1.3 2007-01-06 17:50:22 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/12/02 18:39:06  n-ando
+ * InPortCorbaConsumer class moved to InPortCorbaConsumer.h
+ *
  * Revision 1.1  2006/11/27 09:44:39  n-ando
  * The first commitment.
  *
@@ -27,6 +30,8 @@
 #ifndef InPortConsumer_h
 #define InPortConsumer_h
 
+#include <rtm/CorbaConsumer.h>
+#include <rtm/NVUtil.h>
 namespace RTC
 {
   /*!
@@ -48,11 +53,11 @@ namespace RTC
      * @brief Constructor
      * @endif
      */
-    InPortConsumer();
+    //InPortConsumer();
 
+    
     /*!
      * @if jp
-     *
      * @brief デストラクタ
      * @else
      * @brief Destructor
@@ -62,6 +67,9 @@ namespace RTC
 
     virtual void push() = 0;
 
+    virtual InPortConsumer* clone() const = 0;
+    virtual bool subscribeInterface(const SDOPackage::NVList& properties) = 0;
+    virtual void unsubscribeInterface(const SDOPackage::NVList& properties) = 0;
   protected:
   
   private:
--- rtm/InPortCorbaConsumer.h
+++ rtm/InPortCorbaConsumer.h
@@ -2,7 +2,7 @@
 /*!
  * @file  InPortCorbaConsumer.h
  * @brief InPortCorbaConsumer class
- * @date  $Date: 2006-12-02 18:39:57 $
+ * @date  $Date: 2007-01-06 17:50:57 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: InPortCorbaConsumer.h,v 1.1 2006-12-02 18:39:57 n-ando Exp $
+ * $Id: InPortCorbaConsumer.h,v 1.2 2007-01-06 17:50:57 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/12/02 18:39:57  n-ando
+ * InPortCorbaConsumer class was moved from InPortConsumer.h
+ *
  */
 
 #ifndef InPortCorbaConsumer_h
@@ -28,38 +31,108 @@
 #include <rtm/BufferBase.h>
 #include <rtm/CorbaConsumer.h>
 #include <rtm/InPortConsumer.h>
+#include <iostream>
  
 namespace RTC
 {
   template <class DataType>
   class InPortCorbaConsumer
     : public InPortConsumer,
-      public Consumer<RTC::OutPortAny>
+      public CorbaConsumer<RTC::InPortAny>
   {
   public:
-    InPortCorbaConsumer(BufferBase<DataType>* buffer)
+    InPortCorbaConsumer(BufferBase<DataType>& buffer)
       : m_buffer(buffer)
-    {}
+    {
+    }
+
+
+    InPortCorbaConsumer(const InPortCorbaConsumer<DataType>& consumer)
+      : CorbaConsumer<RTC::InPortAny>(consumer), m_buffer(consumer.m_buffer)
+    {
+    }
+
+    InPortCorbaConsumer&
+    operator=(const InPortCorbaConsumer<DataType>& consumer)
+    {
+      if (this == &consumer) return *this;
+      m_buffer = consumer.m_buffer;
+    }
+
+
     virtual ~InPortCorbaConsumer()
     {}
 
     void put(DataType& data)
     {
-      CORBA::Any* tmp(new CORBA::Any());
+      CORBA::Any tmp;
       tmp <<= data;
-      m_inport->put(tmp);
+      _ptr()->put(tmp);
     }
 
     void push()
     {
-      CORBA::Any* tmp(new CORBA::Any());
-      tmp <<= m_buffer->get();
-      m_inport->put(tmp);
+      DataType data;
+      CORBA::Any tmp;
+      m_buffer.read(data);
+      tmp <<= data;
+
+      //hoge 本当はエラー処理をすべき
+      if (CORBA::is_nil(_ptr())) return;
+      try
+	{
+	  _ptr()->put(tmp);
+	}
+      catch(...)
+	{
+	  //hoge オブジェクトが無効になったらdisconnectすべき
+	  return;
+	}
     }
 
+    virtual InPortCorbaConsumer* clone() const
+    {
+      return new InPortCorbaConsumer<DataType>(*this);
+    }
+
+    virtual bool subscribeInterface(const SDOPackage::NVList& properties)
+    {
+      if (!NVUtil::isStringValue(properties,
+				 "dataport.dataflow_type",
+				 "Push"))
+	{
+	  return false;
+	}
+      
+      CORBA::Long index;
+      index = NVUtil::find_index(properties,
+				 "dataport.corba_any.inport_ref");
+      if (index < 0)
+	{
+	  return false;
+	}
+      
+      CORBA::Object_ptr obj;
+      if (properties[index].value >>= CORBA::Any::to_object(obj))
+	{
+	  if (!CORBA::is_nil(obj))
+	    {
+	      setObject(obj);
+	      return true;
+	    }
+	}
+      return false;;
+    }
+
+
+    virtual void unsubscribeInterface(const SDOPackage::NVList& properties)
+    {
+      ;
+    }
+
+
   private:
-    RTC::InPortAny_var m_inport;
-    BufferBase<DataType>* m_buffer;
+    BufferBase<DataType>& m_buffer;
   };
 };     // namespace RTC
 #endif // InPortCorbaConsumer_h
