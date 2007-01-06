735767420435956a08e41e3609452191983e328b
n-ando <Noriaki.Ando@gmail.com>
2007-01-06 17:58:52 +0000

    Interface and its profile publish functions (publishInterfaceProfile() and publishInterface()) are added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@280 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/OutPortProvider.h
+++ rtm/OutPortProvider.h
@@ -2,7 +2,7 @@
 /*!
  * @file  OutPortProvider.h
  * @brief OutPortProvider class
- * @date  $Date: 2006-11-27 09:44:42 $
+ * @date  $Date: 2007-01-06 17:58:52 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,47 +13,56 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: OutPortProvider.h,v 1.1 2006-11-27 09:44:42 n-ando Exp $
+ * $Id: OutPortProvider.h,v 1.2 2007-01-06 17:58:52 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ *
  */
 
 #ifndef OutPortProvider_h
 #define OutPortProvider_h
 
-#include <rtm/idl/DataPortSkel.h>
-#include <rtm/BufferBase.h>
+#include <rtm/NVUtil.h>
+#include <string>
 
 namespace RTC
 {
   /*!
    * @if jp
-   * @class OutPortProvider
-   * @brief OutPortProvider クラス
+   *
+   * @brief OutPort
+   *
+   * - Port に対して何を提供しているかを宣言する。
+   *   PortProfile の properties に Provider に関する情報を追加する。
+   * (例) OutPort を Provide する場合
+   *
+   * OutPortCorbaProvider が以下を宣言
+   * dataport.interface_type = CORBA_Any
+   * dataport.dataflow_type = Push, Pull
+   * dataport.subscription_type = Once, New, Periodic
+   * 
+   * OutPortRawTCPProviderが以下を宣言
+   * dataport.interface_type = RawTCP
+   * dataport.dataflow_type = Push, Pull
+   * dataport.subscription_type = Once, New, Periodic
+   * 
+   * 最終的に PortProfile::properties は
+   * dataport.interface_type = CORBA_Any, RawTCP
+   * dataport.dataflow_type = Push, Pull
+   * dataport.subscription_type = Once, New, Periodic
+   *
+   *
    * @else
-   * @class OutPortProvider
-   * @brief OutPortProvider class
+   *
+   *
    * @endif
    */
-  template <class DataType>
   class OutPortProvider
-    : public virtual POA_RTC::OutPortAny,
-      public virtual PortableServer::RefCountServantBase
   {
-    /*!
-     * @if jp
-     * @brief コンストラクタ
-     * @else
-     * @brief Constructor
-     * @endif
-     */
-    OutPortProvider(BufferBase<DataType>* buffer)
-      : m_buffer(buffer)
-    {}
-    
+  public:
     /*!
      * @if jp
      * @brief デストラクタ
@@ -61,19 +70,29 @@ namespace RTC
      * @brief Destructor
      * @endif
      */
-    virtual ~OutPortProvider()
-    {}
+    virtual ~OutPortProvider(){};
 
-    CORBA::Any* get()
-    {
-      CORBA::Any_var tmp = new CORBA::Any();
-      tmp <<= m_buffer->read();
-      return tmp._retn();
-    }
+    virtual void publishInterfaceProfile(SDOPackage::NVList& properties);
+
+    virtual void publishInterface(SDOPackage::NVList& properties);
     
-  private:
-    BufferBase<DataType>* m_buffer;
-  };
-};     // namespace RTC
-#endif // OutPortProvider_h
+  protected:
+    void setPortType(const char* port_type);
+    void setDataType(const char* data_type);
+    void setInterfaceType(const char* interface_type);
+    void setDataFlowType(const char* dataflow_type);
+    void setSubscriptionType(const char* subs_type);
 
+    SDOPackage::NVList m_properties;
+
+  private:
+    std::string m_portType;
+    std::string m_dataType;
+    std::string m_interfaceType;
+    std::string m_dataflowType;
+    std::string m_subscriptionType;
+
+  };
+}; // namespace RTC
+
+#endif // OutPortProvider_h
