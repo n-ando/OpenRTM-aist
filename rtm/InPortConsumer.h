d0387bf45dedeb01eaedd5ff0ec022a817159e74
n-ando <Noriaki.Ando@gmail.com>
2007-01-09 09:56:01 +0000

    Include order has changed.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@290 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/InPortConsumer.h
+++ rtm/InPortConsumer.h
@@ -2,7 +2,7 @@
 /*!
  * @file  InPortConsumer.h
  * @brief InPortConsumer class
- * @date  $Date: 2007-01-06 17:50:22 $
+ * @date  $Date: 2007-01-09 09:56:01 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,17 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: InPortConsumer.h,v 1.3 2007-01-06 17:50:22 n-ando Exp $
+ * $Id: InPortConsumer.h,v 1.4 2007-01-09 09:56:01 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2007/01/06 17:50:22  n-ando
+ * Interface subscription/unsubscription functions (subscribeInterface()
+ * and unsubscribeInterface()) are added.
+ * The clone() function to clone the instance is added.
+ *
  * Revision 1.2  2006/12/02 18:39:06  n-ando
  * InPortCorbaConsumer class moved to InPortCorbaConsumer.h
  *
@@ -30,8 +35,9 @@
 #ifndef InPortConsumer_h
 #define InPortConsumer_h
 
-#include <rtm/CorbaConsumer.h>
 #include <rtm/NVUtil.h>
+#include <rtm/CorbaConsumer.h>
+
 namespace RTC
 {
   /*!
