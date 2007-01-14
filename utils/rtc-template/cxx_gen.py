afec3d25dc6f7e509d734cf1fd8ae9d5642de24d
n-ando <Noriaki.Ando@gmail.com>
2007-01-14 23:38:30 +0000

    Updated to use setModuleInitProc() to initialize component locally.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@342 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- utils/rtc-template/cxx_gen.py
+++ utils/rtc-template/cxx_gen.py
@@ -3,7 +3,7 @@
 #
 #  @file cxx_gen.py
 #  @brief rtc-template C++ source code generator class
-#  @date $Date: 2007-01-11 07:46:38 $
+#  @date $Date: 2007-01-14 23:38:30 $
 #  @author Noriaki Ando <n-ando@aist.go.jp>
 # 
 #  Copyright (C) 2004-2007
@@ -13,11 +13,15 @@
 #          Advanced Industrial Science and Technology (AIST), Japan
 #      All rights reserved.
 # 
-#  $Id: cxx_gen.py,v 1.4 2007-01-11 07:46:38 n-ando Exp $
+#  $Id: cxx_gen.py,v 1.5 2007-01-14 23:38:30 n-ando Exp $
 # 
 
 #
 #  $Log: not supported by cvs2svn $
+#  Revision 1.4  2007/01/11 07:46:38  n-ando
+#  Modified for OMG RTC specificatin and OpenRTM-aist-0.4.0.
+#  Almost all template codes were modified according to the new framework.
+#
 #  Revision 1.3  2005/09/08 09:23:55  n-ando
 #  - A bug fix for merge function.
 #
@@ -149,7 +153,6 @@ comp_header = """// -*- C++ -*-
 
 using namespace RTC;
 
-
 class [module.name]
   : public RTC::DataFlowComponentBase
 {
@@ -257,8 +260,6 @@ comp_soruce = """// -*- C++ -*-
 
 #include "[fname_h]"
 
-using namespace std;
-
 // Module specification
 // <rtc-template block="module_spec">
 // </rtc-template>
@@ -327,10 +328,51 @@ comp_compsrc = """// -*- C++ -*-
 void MyModuleInit(RTC::Manager* manager)
 {
   [module.name]Init(manager);
+  RTC::RtcBase* comp;
 
-//  std::string name;
+  // Create a component
+  comp = manager->createComponent("[module.name]");
 
-//  std::cout << "RTComponent: " << name << " was created." << std::endl;
+
+  // Example
+  // The following procedure is examples how handle RT-Components.
+  // These should not be in this function.
+
+  // Get the component's object reference
+//  RTC::RTObject_var rtobj;
+//  rtobj = RTC::RTObject::_narrow(manager->getPOA()->servant_to_reference(comp));
+
+  // Get the port list of the component
+//  PortList* portlist;
+//  portlist = rtobj->get_ports();
+
+  // getting port profiles
+//  std::cout << "Number of Ports: ";
+//  std::cout << portlist->length() << std::endl << std::endl; 
+//  for (CORBA::ULong i(0), n(portlist->length()); i < n; ++i)
+//  {
+//    Port_ptr port;
+//    port = (*portlist)[begin_brace]i[end_brace];
+//    std::cout << "Port" << i << " (name): ";
+//    std::cout << port->get_port_profile()->name << std::endl;
+//    
+//    RTC::PortInterfaceProfileList iflist;
+//    iflist = port->get_port_profile()->interfaces;
+//    std::cout << "---interfaces---" << std::endl;
+//    for (CORBA::ULong i(0), n(iflist.length()); i < n; ++i)
+//    {
+//      std::cout << "I/F name: ";
+//      std::cout << iflist[begin_brace]i[end_brace].instance_name << std::endl;
+//      std::cout << "I/F type: ";
+//      std::cout << iflist[begin_brace]i[end_brace].type_name << std::endl;
+//      const char* pol;
+//      pol = iflist[begin_brace]i[end_brace].polarity == 0 ? "PROVIDED" : "REQUIRED";
+//      std::cout << "Polarity: " << pol << std::endl;
+//    }
+//    std::cout << "---properties---" << std::endl;
+//    NVUtil::dump(port->get_port_profile()->properties);
+//    std::cout << "----------------" << std::endl << std::endl;
+//  }
 
   return;
 }
@@ -339,49 +381,24 @@ int main (int argc, char** argv)
 {
   RTC::Manager* manager;
   manager = RTC::Manager::init(argc, argv);
+
   // Initialize manager
   manager->init(argc, argv);
+
+  // Set module initialization proceduer
+  // This procedure will be invoked in activateManager() function.
+  manager->setModuleInitProc(MyModuleInit);
+
   // Activate manager and register to naming service
   manager->activateManager();
-  manager->getPOAManager()->activate();
-  // Initialize my module on this maneger
-//  manager->initModuleProc(MyModuleInit);
-  // Main loop
-  [module.name]Init(manager);
-  RTC::RtcBase* comp;
-  comp = manager->createComponent("[module.name]");
 
-  RTC::RTObject_var rtobj;
-
-  rtobj = RTC::RTObject::_narrow(manager->getPOA()->servant_to_reference(comp));
-
-  PortList* portlist;
-  portlist = rtobj->get_ports();
-  std::cout << "Number of Portst: " << portlist->length() << std::endl << std::endl; 
-  for (CORBA::ULong i(0), n(portlist->length()); i < n; ++i)
-  {
-    Port_ptr port;
-    port = (*portlist)[begin_brace]i[end_brace];
-    std::cout << "Port" << i << " (name): " << port->get_port_profile()->name << std::endl;
-    
-    RTC::PortInterfaceProfileList iflist;
-    iflist = port->get_port_profile()->interfaces;
-    std::cout << "---interfaces---" << std::endl;
-    for (CORBA::ULong i(0), n(iflist.length()); i < n; ++i)
-    {
-      std::cout << "I/F name: " << iflist[begin_brace]i[end_brace].instance_name << std::endl;
-      std::cout << "I/F type: " << iflist[begin_brace]i[end_brace].type_name << std::endl;
-      const char* pol;
-      pol = iflist[begin_brace]i[end_brace].polarity == 0 ? "PROVIDED" : "REQUIRED";
-      std::cout << "Polarity: " << pol << std::endl;
-    }
-    std::cout << "---properties---" << std::endl;
-    NVUtil::dump(port->get_port_profile()->properties);
-    std::cout << "----------------" << std::endl << std::endl;
-  }
-
-  manager->getORB()->run();
+  // run the manager in blocking mode
+  // runManager(false) is the default.
   manager->runManager();
+
+  // If you want to run the manager in non-blocking mode, do like this
+  // manager->runManager(true);
+
   return 0;
 }
 """
@@ -623,7 +640,7 @@ def MakeConsumerIDL(dict):
 def MakeActivityFuncs(dict):
 	acts = (("onInitialize",  ""), \
 		("onFinalize",    ""), \
-		("onStaertup",    "RTC::UniqueId ec_id"), \
+		("onStartup",     "RTC::UniqueId ec_id"), \
 		("onShutdown",    "RTC::UniqueId ec_id"), \
 		("onActivated",   "RTC::UniqueId ec_id"), \
 		("onDeactivated", "RTC::UniqueId ec_id"), \
