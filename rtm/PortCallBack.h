9749d2a3a2348dd711c3563ccd6d074198cc7e17
n-ando <Noriaki.Ando@gmail.com>
2007-01-21 12:56:35 +0000

    virtual dtors were defined.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@365 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/PortCallBack.h
+++ rtm/PortCallBack.h
@@ -2,7 +2,7 @@
 /*!
  * @file PortCallBack.h
  * @brief PortCallBack class
- * @date $Date: 2006-12-02 18:51:18 $
+ * @date $Date: 2007-01-21 12:56:35 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: PortCallBack.h,v 1.1 2006-12-02 18:51:18 n-ando Exp $
+ * $Id: PortCallBack.h,v 1.2 2007-01-21 12:56:35 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/12/02 18:51:18  n-ando
+ * The first commitment.
+ *
  *
  */
 
@@ -46,6 +49,7 @@ namespace RTC
   template <class DataType>
   struct OnWrite
   {
+    virtual ~OnWrite(){}
     virtual void operator()(const DataType& value) = 0;
   };
   
@@ -66,6 +70,7 @@ namespace RTC
   template <class DataType>
   struct OnWriteConvert
   {
+    virtual ~OnWriteConvert(){}
     virtual DataType operator()(const DataType& value) = 0;
   };
   
@@ -86,6 +91,7 @@ namespace RTC
   template <class DataType>
   struct OnRead
   {
+    virtual ~OnRead(){}
     virtual void operator()() = 0;
   };
   
@@ -106,6 +112,7 @@ namespace RTC
   template <class DataType>
   struct OnReadConvert
   {
+    virtual ~OnReadConvert(){}
     virtual DataType operator()(const DataType& value) = 0;
   };
   
@@ -126,6 +133,7 @@ namespace RTC
   template <class DataType>
   struct OnOverflow
   {
+    virtual ~OnOverflow(){}
     virtual void operator()(const DataType& value) = 0;
   };
   
@@ -145,6 +153,7 @@ namespace RTC
   template <class DataType>
   struct OnUnderflow
   {
+    virtual ~OnUnderflow(){}
     virtual DataType operator()() = 0;
   };
   
