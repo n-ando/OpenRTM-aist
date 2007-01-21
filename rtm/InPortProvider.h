ca5acb0c6a40321c3fcb16999e992c715c486f59
n-ando <Noriaki.Ando@gmail.com>
2007-01-21 09:49:52 +0000

    The destructor was moved to cpp file.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@356 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/InPortProvider.h
+++ rtm/InPortProvider.h
@@ -2,7 +2,7 @@
 /*!
  * @file  InPortProvider.h
  * @brief InPortProvider class
- * @date  $Date: 2007-01-06 17:53:42 $
+ * @date  $Date: 2007-01-21 09:49:52 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -13,12 +13,16 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: InPortProvider.h,v 1.3 2007-01-06 17:53:42 n-ando Exp $
+ * $Id: InPortProvider.h,v 1.4 2007-01-21 09:49:52 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2007/01/06 17:53:42  n-ando
+ * Interface and its profile publish functions (publishInterfaceProfile()
+ * and publishInterface()) are added.
+ *
  * Revision 1.2  2006/12/02 18:41:51  n-ando
  * A trivial fix.
  *
@@ -63,7 +67,7 @@ namespace RTC
      * @brief Destructor
      * @endif
      */
-    virtual ~InPortProvider(){};
+    virtual ~InPortProvider();
 
     virtual void publishInterfaceProfile(SDOPackage::NVList& properties);
 
@@ -82,8 +86,6 @@ namespace RTC
     std::string m_interfaceType;
     std::string m_dataflowType;
     std::string m_subscriptionType;
-
-
 
   };
  
