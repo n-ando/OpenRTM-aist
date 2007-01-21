77e35f26edd4e118d77244c89a3f95b8ff033ad4
n-ando <Noriaki.Ando@gmail.com>
2007-01-21 13:05:21 +0000

    Some trivial fixes.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@368 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/SdoOrganization.h
+++ rtm/SdoOrganization.h
@@ -2,7 +2,7 @@
 /*!
  * @file SdoOrganization.h
  * @brief SDO Organization implementation class
- * @date $Date: 2006-10-30 08:10:54 $
+ * @date $Date: 2007-01-21 13:05:21 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -12,12 +12,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: SdoOrganization.h,v 1.3 2006-10-30 08:10:54 n-ando Exp $
+ * $Id: SdoOrganization.h,v 1.4 2007-01-21 13:05:21 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2006/10/30 08:10:54  n-ando
+ * CORBA sequence operations were replaced by CORBA_SeqUtil functions.
+ *
  * Revision 1.2  2006/10/17 10:12:57  n-ando
  * Small fixes.
  *
@@ -30,10 +33,11 @@
 #ifndef SdoOrganization_h
 #define SdoOrganization_h
 
-#include "rtm/RTC.h"
-
-#include "rtm/idl/SDOPackageSkel.h"
-
+#include <rtm/RTC.h>
+#include <rtm/idl/SDOPackageSkel.h>
+#include <string>
+#include <ace/Guard_T.h>
+#include <ace/Thread_Mutex.h>
 
 // SdoOrganization.o 23788
 // 41892
