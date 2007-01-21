3dae9315e76d6f9c30ce071d1fc606475a88b4fb
n-ando <Noriaki.Ando@gmail.com>
2007-01-21 06:46:34 +0000

    Now kterm/xterm/gnome-teminal are supported.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@348 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SimpleIO/run.sh
+++ examples/SimpleIO/run.sh
@@ -1,8 +1,40 @@
 #!/bin/sh
+#
+# @file run.sh
+# @brief SimpleIO example startup script
+# @date $Date: 2007-01-21 06:46:34 $
+#
+# Copyright (c) 2003-2007 Noriaki Ando <n-ando@aist.go.jp>
+#          Task-intelligence Research Group,
+#          Intelligent System Research Institute,
+#          National Institute of Industrial Science (AIST), Japan
+#          All rights reserved.
+#
+# $Id: run.sh,v 1.3 2007-01-21 06:46:34 n-ando Exp $
+#
 
 nsport='9876'
 hostname=`hostname`
 
+
+term=`which kterm`
+
+if test "x$term" = "x" ; then
+    term=`which xterm`
+fi
+
+if test "x$term" = "x" ; then
+    term=`which uxterm`
+fi
+
+if test "x$term" = "x" ; then
+    term=`which gnome-terminal`
+fi
+
+if test "x$term" = "x" ; then
+    echo "No terminal program (kterm/xterm/gnome-terminal) exists."
+    exit
+fi
 
 ../../utils/rtm-naming/rtm-naming $nsport
 
@@ -10,14 +42,13 @@ echo 'corba.nameservers: '$hostname':'$nsport > ./rtc.conf
 echo 'naming_formats: %n.rtc' >> ./rtc.conf
 echo 'logger.log_level: TRACE' >> ./rtc.conf
 
-kterm -e ./ConsoleInComp -f ./rtc.conf &
-kterm -e ./ConsoleOutComp -f ./rtc.conf &
+$term -e ./ConsoleInComp &
+$term -e ./ConsoleOutComp &
 
 sleep 5
-./ConnectorComp -f ./rtc.conf &
-sleep 10
+./ConnectorComp &
 
-nspid=`ps -ax | grep 9876 | awk '{print $1}'`
-
-kill $nspid
-echo 'Naming service was stopped.'
+#sleep 10
+#nspid=`ps -ax | grep 9876 | awk '{print $1}'`
+#kill $nspid
+#echo 'Naming service was stopped.'
