864c281c582e88c26a6173abc691fd22bbbba45c
n-ando <Noriaki.Ando@gmail.com>
2007-01-21 06:45:42 +0000

    Some subscription types are supported.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@347 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SimpleIO/ConnectorComp.cpp
+++ examples/SimpleIO/ConnectorComp.cpp
@@ -2,7 +2,7 @@
 /*!
  * @file ConnectorComp.cpp
  * @brief connector application
- * @date $Date: 2007-01-21 05:51:03 $
+ * @date $Date: 2007-01-21 06:45:42 $
  *
  * Copyright (c) 2003-2007 Noriaki Ando <n-ando@aist.go.jp>
  *          Task-intelligence Research Group,
@@ -10,11 +10,12 @@
  *          National Institute of Industrial Science (AIST), Japan
  *          All rights reserved.
  *
- * $Id: ConnectorComp.cpp,v 1.4 2007-01-21 05:51:03 n-ando Exp $
+ * $Id: ConnectorComp.cpp,v 1.5 2007-01-21 06:45:42 n-ando Exp $
  */
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <rtm/CorbaNaming.h>
 #include <rtm/RTObject.h>
 #include <rtm/NVUtil.h>
@@ -25,9 +26,55 @@
 
 using namespace RTC;
 
+void usage()
+{
+  std::cout << std::endl;
+  std::cout << "usage: ConnectorComp [options]" << std::endl;
+  std::cout << std::endl;
+  std::cout << "  --flush         ";
+  std::cout << ": Set subscription type Flush" << std::endl;
+  std::cout << "  --new           ";
+  std::cout << ": Set subscription type New" << std::endl;
+  std::cout << "  --periodic [Hz] ";
+  std::cout << ": Set subscription type Periodic" << std::endl;	
+  std::cout << std::endl;
+}
+
 int main (int argc, char** argv)
 {
-  CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
+  int _argc(0);
+  char** _argv(0);
+
+  std::string subs_type;
+  std::string period;
+  for (int i = 1; i < argc; ++i)
+    {
+      std::string arg(argv[i]);
+      if (arg == "--flush")         subs_type = "Flush";
+      else if (arg == "--new")      subs_type = "New";
+      else if (arg == "--periodic")
+	{
+	  subs_type = "Periodic";
+	  if (++i < argc) period = argv[i];
+	  else            period = "1.0";
+	}
+      else if (arg == "--help")
+	{
+	  usage();
+	  exit(1);
+	}
+      else
+	{
+	  subs_type = "Flush";
+	}
+    }
+  
+  std::cout << "Subscription Type: " << subs_type << std::endl;
+  if (period != "")
+    std::cout << "Period: " << period << " [Hz]" << std::endl;
+
+
+  CORBA::ORB_var orb = CORBA::ORB_init(_argc, _argv);
   CorbaNaming naming(orb, "localhost:9876");
 
   CorbaConsumer<RTObject> conin, conout;
@@ -39,17 +86,18 @@ int main (int argc, char** argv)
 
   // get ports
   pin = conin->get_ports();
+  pin[0]->disconnect_all();
   assert(pin->length() > 0);
   // activate ConsoleIn0
   ExecutionContextServiceList_var eclisti;
   eclisti = conin->get_execution_context_services();
   eclisti[0]->activate_component(RTObject::_duplicate(conin._ptr()));
 
-
   // find ConsoleOut0 component
   conout.setObject(naming.resolve("ConsoleOut0.rtc"));
   // get ports
   pout = conout->get_ports();
+  pout[0]->disconnect_all();
   assert(pout->length() > 0);
   // activate ConsoleOut0
   ExecutionContextServiceList_var eclisto;
@@ -71,7 +119,11 @@ int main (int argc, char** argv)
 					 "Push"));
   CORBA_SeqUtil::push_back(prof.properties,
 			   NVUtil::newNV("dataport.subscription_type",
-					 "Flush"));
+					 subs_type.c_str()));
+  if (period != "")
+  CORBA_SeqUtil::push_back(prof.properties,
+			   NVUtil::newNV("dataport.push_interval",
+					 period.c_str()));
   ReturnCode_t ret;
   ret = pin[0]->connect(prof);
   assert(ret == RTC::OK);
