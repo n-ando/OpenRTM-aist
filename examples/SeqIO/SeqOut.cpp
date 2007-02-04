1756badbceae43ccd68dd76de578b7f90a347c30
n-ando <Noriaki.Ando@gmail.com>
2007-02-04 16:48:17 +0000

    Updated for 0.4.0.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@403 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SeqIO/SeqOut.cpp
+++ examples/SeqIO/SeqOut.cpp
@@ -1,234 +1,216 @@
 // -*- C++ -*-
 /*!
- * @file SeqOut.cpp
- * @brief SeqOut component
- * @date $Date: 2005-05-12 09:06:20 $
+ * @file  SeqOut.cpp
+ * @brief Sequence OutPort component
+ * $Date: 2007-02-04 16:47:59 $
  *
- * $Id: SeqOut.cpp,v 1.1.1.1 2005-05-12 09:06:20 n-ando Exp $
+ * $Id: SeqOut.cpp,v 1.2 2007-02-04 16:47:59 n-ando Exp $
  */
 
-
-
 #include "SeqOut.h"
+#include <stdlib.h>
 
-using namespace std;
+// Module specification
+// <rtc-template block="module_spec">
+static const char* seqout_spec[] =
+  {
+    "implementation_id", "SeqOut",
+    "type_name",         "SequenceOutComponent",
+    "description",       "Sequence OutPort component",
+    "version",           "1.0",
+    "vendor",            "Noriaki Ando, AIST",
+    "category",          "example",
+    "activity_type",     "DataFlowComponent",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
 
-SeqOut::SeqOut(RtcManager* manager)
-  : RtcBase(manager) ,
-	m_DoubleOut("Double", m_Double),
-    m_FloatOut("Float", m_Float),
+SeqOut::SeqOut(RTC::Manager* manager)
+  : RTC::DataFlowComponentBase(manager),
+    // <rtc-template block="initializer">
+    m_ShortOut("Short", m_Short),
     m_LongOut("Long", m_Long),
-    m_ShortOut("Short", m_Short)
+    m_FloatOut("Float", m_Float),
+    m_DoubleOut("Double", m_Double),
+    m_ShortSeqOut("ShortSeq", m_ShortSeq),
+    m_LongSeqOut("LongSeq", m_LongSeq),
+    m_FloatSeqOut("FloatSeq", m_FloatSeq),
+    m_DoubleSeqOut("DoubleSeq", m_DoubleSeq),
+    
+    // </rtc-template>
+	dummy(0)
 {
-  registerOutPort(m_DoubleOut);
-  registerOutPort(m_FloatOut);
-  registerOutPort(m_LongOut);
-  registerOutPort(m_ShortOut);
-
-  m_Double.data.length(10);
-  m_Float.data.length(10);
-  m_Long.data.length(10);
-  m_Short.data.length(10);
-
-  appendAlias("example/SeqOut|rtc");
-}
-
-/*
-RtmRes SeqOut::rtc_init_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_ready_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_ready_do()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_ready_exit()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_starting_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_active_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-
-RtmRes SeqOut::rtc_active_do()
-{
-  int r;
-  r = rand();
-
-  for (unsigned long int i = 0; i < 10; i++)
-	{
-	  m_Double.data[i] = rand();
-	  m_Float.data[i] = rand();
-	  m_Long.data[i] = rand();
-	  m_Short.data[i] = rand();
-	  std::cout << i << ": ";
-	  std::cout << m_Double.data[i] << " ";
-	  std::cout << m_Float.data[i] << " ";
-	  std::cout << m_Long.data[i] << " ";
-	  std::cout << m_Short.data[i] << endl;
-	  
-	}
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
   
-  std::cout << "[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r";
+  // Set OutPort buffer
+  registerOutPort("Short", m_ShortOut);
+  registerOutPort("Long", m_LongOut);
+  registerOutPort("Float", m_FloatOut);
+  registerOutPort("Double", m_DoubleOut);
+  registerOutPort("ShortSeq", m_ShortSeqOut);
+  registerOutPort("LongSeq", m_LongSeqOut);
+  registerOutPort("FloatSeq", m_FloatSeqOut);
+  registerOutPort("DoubleSeq", m_DoubleSeqOut);
   
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  m_DoubleSeq.data.length(10);
+  m_FloatSeq.data.length(10);
+  m_LongSeq.data.length(10);
+  m_ShortSeq.data.length(10);
+}
+
+SeqOut::~SeqOut()
+{
+}
+
+
+/*
+RTC::ReturnCode_t SeqOut::onInitialize()
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onFinalize()
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+
+RTC::ReturnCode_t SeqOut::onExecute(RTC::UniqueId ec_id)
+{
+  m_Short.data = rand();
+  m_Long.data = rand();
+  m_Float.data = rand();
+  m_Double.data = rand();
+
+  std::cout << "-: ";
+  std::cout << m_Double.data << " ";
+  std::cout << m_Float.data << " ";
+  std::cout << m_Long.data << " ";
+  std::cout << m_Short.data << std::endl << std::endl;
+
+  for (CORBA::ULong i = 0; i < 10; ++i)
+    {
+      m_DoubleSeq.data[i] = static_cast<double>(rand());
+      m_FloatSeq.data[i] = static_cast<float>(rand());
+      m_LongSeq.data[i] = static_cast<long>(rand());
+      m_ShortSeq.data[i] = static_cast<short>(rand());
+      std::cout << i << ": ";
+      std::cout << m_DoubleSeq.data[i] << " ";
+      std::cout << m_FloatSeq.data[i] << " ";
+      std::cout << m_LongSeq.data[i] << " ";
+      std::cout << m_ShortSeq.data[i] << std::endl;
+    }
+  
+  std::cout << "[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r[A\r";
+
   m_DoubleOut.write();
   m_FloatOut.write();
   m_LongOut.write();
   m_ShortOut.write();
-  usleep(1000);
-  return RTM_OK;
+
+  m_DoubleSeqOut.write();
+  m_FloatSeqOut.write();
+  m_LongSeqOut.write();
+  m_ShortSeqOut.write();
+
+  usleep(10000);
+
+  return RTC::OK;
 }
 
 
-
+/*
+RTC::ReturnCode_t SeqOut::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
 
 /*
-RtmRes SeqOut::rtc_active_exit()
+RTC::ReturnCode_t SeqOut::onError(RTC::UniqueId ec_id)
 {
-  return RTM_OK;
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t SeqOut::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::OK;
 }
 */
 
 
 
-/*
-RtmRes SeqOut::rtc_stopping_entry()
+extern "C"
 {
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_aborting_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_error_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_error_do()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_error_exit()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_fatal_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_fatal_do()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_fatal_exit()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-/*
-RtmRes SeqOut::rtc_exiting_entry()
-{
-  return RTM_OK;
-}
-*/
-
-
-
-extern "C" {
-  
-  RtcBase* SeqOutNew(RtcManager* manager)
+ 
+  void SeqOutInit(RTC::Manager* manager)
   {
-	return new SeqOut(manager);
+    RTC::Properties profile(seqout_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<SeqOut>,
+                             RTC::Delete<SeqOut>);
   }
   
-  
-  void SeqOutDelete(RtcBase* p)
-  {
-	delete (SeqOut *)p;
-	return;
-  }
-  
-  
-  void SeqOutInit(RtcManager* manager)
-  {
-	RtcModuleProfile profile(seqout_spec);
-	manager->registerComponent(profile, SeqOutNew, SeqOutDelete);
-  }
 };
+
 
