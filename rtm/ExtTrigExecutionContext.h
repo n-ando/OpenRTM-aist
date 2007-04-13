4d0525c04effe7cac3047dc317e6ecb12fb2faa5
n-ando <Noriaki.Ando@gmail.com>
2007-04-13 16:08:24 +0000

    External Triggered ExecutionContext class.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@449 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ rtm/ExtTrigExecutionContext.h
@@ -0,0 +1,63 @@
+// -*- C++ -*-
+/*!
+ * @file ExtTrigExecutionContext.h
+ * @brief ExtTrigExecutionContext class
+ * @date $Date: 2007-04-13 16:08:24 $
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2007
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id: ExtTrigExecutionContext.h,v 1.1 2007-04-13 16:08:24 n-ando Exp $
+ *
+ */
+
+/*
+ * $Log: not supported by cvs2svn $
+ */
+
+#ifndef ExtTrigExecutionContext_h
+#define ExtTrigExecutionContext_h
+
+#include <ace/Task.h>
+#include <ace/Synch.h>
+
+#include <rtm/Manager.h>
+#include <rtm/PeriodicExecutionContext.h>
+
+namespace RTC
+{
+  class ExtTrigExecutionContext
+    : public virtual PeriodicExecutionContext
+  {
+  public:
+    ExtTrigExecutionContext();
+    virtual ~ExtTrigExecutionContext();
+    virtual void tick();
+    virtual int svc(void);
+
+  private:
+    struct Worker
+    {
+      Worker() : _cond(_mutex), _called(false) {};
+      ACE_Thread_Mutex _mutex;
+      ACE_Condition<ACE_Thread_Mutex> _cond;
+      bool _called;
+    };
+    // A condition variable for external triggered worker
+    Worker m_worker;
+
+  };
+};
+
+extern "C"
+{
+  void ExtTrigExecutionContextInit(RTC::Manager* manager);
+};
+
+#endif // ExtTrigExecutionContext_h
+
