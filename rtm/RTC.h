0a657a8e4fdf571e0a4dae11dbe5151b625a3364
n-ando <Noriaki.Ando@gmail.com>
2007-07-20 16:05:14 +0000

    usleep definition for win32 porting. Useless __declspec definitions were deleted.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_0_4@528 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/RTC.h
+++ rtm/RTC.h
@@ -2,7 +2,7 @@
 /*!
  * @file RTC.h
  * @brief RTComponent header
- * @date $Date: 2007-04-13 18:14:41 $
+ * @date $Date: 2007-07-20 16:05:14 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2003-2005
@@ -12,12 +12,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: RTC.h,v 1.5 2007-04-13 18:14:41 n-ando Exp $
+ * $Id: RTC.h,v 1.5.2.1 2007-07-20 16:05:14 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2007/04/13 18:14:41  n-ando
+ * Now OpenRTMSkel.h is included.
+ *
  * Revision 1.4  2007/01/21 10:38:54  n-ando
  * Some tirivial fixex.
  *
@@ -63,11 +66,11 @@
 // for windows DLL
 #ifdef WIN32
 
-#ifdef DLLEXT
-#define EXPORTS __declspec(dllexport)
-#else  // DLLEXT
-#define EXPORTS __declspec(dllimport)
-#endif // DLLEXT
+//#ifdef DLLEXT
+//#define EXPORTS __declspec(dllexport)
+//#else  // DLLEXT
+//#define EXPORTS __declspec(dllimport)
+//#endif // DLLEXT
 
 #define LINE_MAX 256
 BOOL WINAPI DllMain (HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved);
@@ -80,4 +83,10 @@ BOOL WINAPI DllMain (HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved);
 #endif // WIN32
 
 #include "rtm/idl/OpenRTMSkel.h"
+
+
+#ifdef WIN32
+#define usleep(x) Sleep(x/1000);
+#endif // WIN32
+
 #endif // RTC_h
