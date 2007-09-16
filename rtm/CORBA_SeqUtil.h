566ac883189ef8c17fefe33549852e89cfd571c4
n-ando <Noriaki.Ando@gmail.com>
2007-09-16 08:19:02 +0000

    The problem of redefinition of CORBA::ULong and CORBA::Long was modified.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_0_4@564 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- rtm/CORBA_SeqUtil.h
+++ rtm/CORBA_SeqUtil.h
@@ -2,7 +2,7 @@
 /*!
  * @file CORBA_SeqUtil.h
  * @brief CORBA sequence utility template functions
- * @date $Date: 2006-11-02 09:04:39 $
+ * @date $Date: 2007-09-16 08:19:02 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * Copyright (C) 2006
@@ -12,12 +12,15 @@
  *         Advanced Industrial Science and Technology (AIST), Japan
  *     All rights reserved.
  *
- * $Id: CORBA_SeqUtil.h,v 1.3 2006-11-02 09:04:39 n-ando Exp $
+ * $Id: CORBA_SeqUtil.h,v 1.3.4.1 2007-09-16 08:19:02 n-ando Exp $
  *
  */
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2006/11/02 09:04:39  n-ando
+ * CORBA_SeqUtil::insert()'s bug was fixed.
+ *
  * Revision 1.2  2006/10/30 08:09:07  n-ando
  * Two functions push_back_lsit(), erase_if() were added.
  * Function reference manual was revised.
@@ -31,11 +34,7 @@
 #ifndef CORBA_SeqUtil_h
 #define CORBA_SeqUtil_h
 
-namespace CORBA
-{
-  typedef unsigned long ULong;
-  typedef long Long;
-};
+#include <rtm/RTC.h>
 
 /*!
  * @if jp
