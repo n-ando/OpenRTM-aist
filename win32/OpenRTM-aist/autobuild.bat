37456aceeeac5298e63bdaf64d7871096cbeae4e
n-ando <Noriaki.Ando@gmail.com>
2008-02-09 20:04:06 +0000

    For wix's wxs file generation.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_0_4@643 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- win32/OpenRTM-aist/autobuild.bat
+++ win32/OpenRTM-aist/autobuild.bat
@@ -1,6 +1,6 @@
 @rem
 @rem @brief Visual Studio automatic build script
-@rem @date $Date: 2008-01-25 10:53:30 $
+@rem @date $Date: 2008-02-09 20:01:28 $
 @rem @author Norkai Ando <n-ando@aist.go.jp>
 @rem
 @rem Copyright (C) 2008
@@ -11,10 +11,13 @@
 @rem         Advanced Industrial Science and Technology (AIST), Japan
 @rem     All rights reserved.
 @rem
-@rem $Id: autobuild.bat,v 1.1.2.1 2008-01-25 10:53:30 n-ando Exp $
+@rem $Id: autobuild.bat,v 1.1.2.2 2008-02-09 20:01:28 n-ando Exp $
 @rem
 
 @rem $Log: not supported by cvs2svn $
+@rem Revision 1.1.2.1  2008/01/25 10:53:30  n-ando
+@rem Automatic build script for Visual Studio
+@rem
 
 @set RTM_ROOT=%~dp0
 @set PATH=C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727;"C:\Program Files\Microsoft Visual Studio 8\Common7\IDE";%PATH%
@@ -23,8 +26,8 @@
 @rem build OpenRTM-aist
 @rem ============================================================
 
-msbuild OpenRTM-aist.sln /t:Rebuild /p:Configuration=Release > build.log
-msbuild OpenRTM-aist.sln /t:Rebuild /p:Configuration=Debug >> builds.log
+devenv.com OpenRTM-aist.sln /rebuild Release /projectconfig Release > build.log
+devenv.com OpenRTM-aist.sln /rebuild Debug /projectconfig Debug >> build.log
 
 @rem ============================================================
 @rem build USBCamera examples
@@ -34,3 +37,6 @@ devenv.com examples\USBCamera\USBCameraMonitor.vcproj /rebuild Release /projectc
 devenv.com examples\USBCamera\USBCameraAcquire.vcproj /rebuild Debug /projectconfig Debug >> build.log
 devenv.com examples\USBCamera\USBCameraMonitor.vcproj /rebuild Debug /projectconfig Debug >> build.log
 
+
+cd installer
+call autowix.cmd
