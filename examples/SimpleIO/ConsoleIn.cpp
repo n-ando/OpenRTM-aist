74b276f2b621536cfaecfc837783e429779dcc99
n-ando <Noriaki.Ando@gmail.com>
2009-01-12 09:33:48 +0000

    An unnecessary comment has been removed.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@1118 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SimpleIO/ConsoleIn.cpp
+++ examples/SimpleIO/ConsoleIn.cpp
@@ -77,7 +77,6 @@ extern "C"
  
   void ConsoleInInit(RTC::Manager* manager)
   {
-//    RTC::Properties profile(consolein_spec);
     coil::Properties profile(consolein_spec);
     manager->registerFactory(profile,
                              RTC::Create<ConsoleIn>,
