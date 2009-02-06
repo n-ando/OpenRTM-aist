50c7f11b047b3878383b1bbb6e0b5ad156b1f68b
n-ando <Noriaki.Ando@gmail.com>
2009-02-06 09:01:09 +0000

    Vine needs e2fsprogs e2fsprogs-devel for libuuid.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@1191 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- build/pkg_install_vine.sh
+++ build/pkg_install_vine.sh
@@ -25,7 +25,8 @@ openrtm_repo="rpm     http://www.openrtm.org/pub/Linux/Vine/apt $vinever/\$(ARCH
 omni="omniORB omniORB-devel omniORB-servers omniORB-doc"
 ace="ace ace-devel"
 openrtm="OpenRTM-aist OpenRTM-aist-devel OpenRTM-aist-doc OpenRTM-aist-example PyYAML"
-packages="$omni $ace $openrtm"
+libuuid="e2fsprogs e2fsprogs-devel"
+packages="$libuuid $omni $ace $openrtm"
 
 
 
