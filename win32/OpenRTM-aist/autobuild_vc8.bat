614e02e3b94e87d8ba7da16596d858fb593400d9
fsi-katami <openrtm@openrtm.org>
2009-04-10 10:54:15 +0000

    Some link errors of win32 have been solved.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@1264 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- win32/OpenRTM-aist/autobuild_vc8.bat
+++ win32/OpenRTM-aist/autobuild_vc8.bat
@@ -21,9 +21,8 @@
 @rem copy property sheet
 @rem ============================================================
 copy   etc\rtm_config_omni407.vsprops rtm_config.vsprops
-copy   etc\coil_config_omni407.vsprops coil_config.vsprops
 copy   etc\rtm_config_omni407.vsprops examples\USBCamera\rtm_config.vsprops
-copy   etc\coil_config_omni407.vsprops examples\USBCamera\coil_config.vsprops
+copy   coil_config.vsprops examples\USBCamera\coil_config.vsprops
 
 @rem ============================================================
 @rem build OpenRTM-aist
