6b0ae1ffab099024628bf52497bdbd83c00f9ea7
n-ando <Noriaki.Ando@gmail.com>
2009-07-24 01:49:57 +0000

    Logleve NORMAL has been abolished.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@1475 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/DefaultConfiguration.h
+++ src/lib/rtm/DefaultConfiguration.h
@@ -53,7 +53,7 @@ static const char* default_config[] =
     "logger.enable",                    "YES",
     "logger.file_name",                 "./rtc%p.log",
     "logger.date_format",               "%b %d %H:%M:%S",
-    "logger.log_level",                 "NORMAL",
+    "logger.log_level",                 "INFO",
     "logger.stream_lock",               "NO",
     "logger.master_logger",             "",
     "module.conf_path",                 "",
