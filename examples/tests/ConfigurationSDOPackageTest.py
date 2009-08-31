158c0de50164fce81d1a6512a7363f8dc68b6e0f
hakuta <openrtm@openrtm.org>
2009-08-31 07:21:12 +0000

    Improved memory leak check tool.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@1541 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/tests/ConfigurationSDOPackageTest.py
+++ examples/tests/ConfigurationSDOPackageTest.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: euc-jp -*-
 #
 ## ConfigurationSDOPackageTest.py
 ##
-## уГбуГвуГкуГ╝уГкуГ╝уВпуГБуВзуГГуВп
-## SDOPackage.idlуБзхоЪч╛йуБХуВМуБжуБДуВЛуВкуГЪуГмуГ╝уВ╖уГзуГ│
-## ConfigurationуБлщЦвуБЩуВЛуВкуГЪуГмуГ╝уВ╖уГзуГ│
+## есетеъб╝еъб╝епе┴езе├еп
+## SDOPackage.idlд╟─ъ╡┴д╡дьд╞дддыеке┌еьб╝е╖ечеє
+## Configurationд╦┤╪д╣дыеке┌еьб╝е╖ечеє
 #
 # $Id$
 #
