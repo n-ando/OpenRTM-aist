e71b6218b9d3cb855f09d5687ee0a3a35af5669f
n-ando <Noriaki.Ando@gmail.com>
2010-01-09 07:10:43 +0000

    coil Process functions have been implemented.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@1623 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/coil/posix/coil/Process.cpp
@@ -0,0 +1,54 @@
+// -*- C++ -*-
+/*!
+ * @file  Process.h
+ * @brief dleware Service interface
+ * @date  $Date$
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2010
+ *     Noriaki Ando
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef COIL_PROCESS_H
+#define COIL_PROCESS_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <coil/stringutil.h>
+
+namespace coil
+{
+
+  /*!
+   * @if jp
+   * @brief プロセスを起動する
+   * @else
+   * @brief Launching a process
+   * @endif
+   */
+  int launch_shell(std::string command)
+  {
+    pid_t pid;
+    pid = fork();
+    
+    if (pid == 0) // I'm child process
+      {
+        coil::vstring vstr(::coil::split(command, " "));
+        char* const * argv = ::coil::toArgv(vstr);
+
+        execvp(vstr.front().c_str(), argv);
+        return -1;
+      }
+    return 0;
+  }
+}; // namespace coil
+#endif // COIL_PROCESS_H
