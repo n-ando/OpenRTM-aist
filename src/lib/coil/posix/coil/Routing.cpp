e4f2ddd5c6d7234adfcbc1286d73c8f6cca432c5
n-ando <Noriaki.Ando@gmail.com>
2010-01-10 08:56:59 +0000

    A function to find endpoint from destination address has been added. The win32 version has not been tested yet.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@1625 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/coil/posix/coil/Routing.cpp
@@ -0,0 +1,139 @@
+// -*- C++ -*-
+/*!
+ * @file  Routing.cpp
+ * @brief Network routing information handling functions
+ * @date  $Date$
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2010
+ *     Noriaki Ando
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id$
+ *
+ */
+
+#include <stdio.h>
+
+#include <coil/Routing.h>
+#include <coil/stringutil.h>
+#include <coil/config_coil.h>
+
+namespace coil
+{
+  /*!
+   * @if jp
+   * @brief 宛先アドレスから利用されるエンドポイントアドレスを得る
+   * @else
+   * @brief Getting network interface name from destination address
+   * @endif
+   */
+  bool dest_to_endpoint(std::string dest_addr, std::string& endpoint)
+  {
+    std::string dest_if;
+    if (!find_dest_ifname(dest_addr, dest_if))
+      {
+        return false;
+      }
+    return ifname_to_ipaddr(dest_if, endpoint);
+  }
+
+  /*!
+   * @if jp
+   * @brief 宛先アドレスから利用されるネットワークインターフェース名を得る
+   * @else
+   * @brief Getting network interface name from destination address
+   * @endif
+   */
+  bool find_dest_ifname(std::string dest_addr, std::string& dest_if)
+  {
+    //  struct hostent ipaddr;
+    //  ipaddr = gethostbyname(dest_addr.c_str());
+    
+#ifdef COIL_OS_FREEBSD
+    std::string cmd("route get ");
+    const char* match_str = "interface";
+    const char* delimiter = ":";
+    int ifname_pos(1);
+    cmd += dest_addr;
+#endif // COIL_OS_IS_FREEBSD
+#ifdef COIL_OS_LINUX    
+    std::string cmd("ip route get ");
+    const char* match_str = "dev ";
+    const char* delimiter = " ";
+    int ifname_pos(2);
+    cmd += dest_addr;
+#endif // COIL_OS_IS_LINUX    
+    
+    FILE* fp;
+    if ((fp = popen(cmd.c_str(), "r")) == NULL)
+      {
+        return false;
+      }
+    
+    do
+      {
+        char str[512];
+        fgets(str, 512, fp);
+        std::string line(str);
+        
+        if (std::string::npos == line.find(match_str)) { continue; }
+        
+        line.erase(line.end() - 1);
+        coil::vstring vs(coil::split(line, delimiter));
+        if (vs.size() == 2)
+          {
+            dest_if = vs[ifname_pos];
+            pclose(fp);
+            return true;
+          }
+      } while (!feof(fp));
+    pclose(fp);
+    return false;
+  }
+
+  /*!
+   * @if jp
+   * @brief ネットワークインターフェース名からIPアドレスを得る
+   * @else
+   * @brief Get IP address from a network interface name
+   * @endif
+   */
+  bool ifname_to_ipaddr(std::string ifname, std::string& ipaddr)
+  {
+    std::string cmd("ifconfig ");
+    cmd += ifname;
+    
+    FILE* fp;
+    if ((fp = popen(cmd.c_str(), "r")) == NULL)
+      {
+        return false;
+      }
+    
+    do
+      {
+        char str[512];
+        fgets(str, 512, fp);
+        std::string line(str);
+        
+        if (std::string::npos == line.find("inet ")) { continue; }
+        
+        line.erase(line.end() - 1);
+        coil::eraseHeadBlank(line);
+        coil::vstring vs(coil::split(line, " "));
+        if (vs.size() == 6)
+          {
+            ipaddr = vs[1];
+            pclose(fp);
+            return true;
+          }
+      } while (!feof(fp));
+    pclose(fp);
+    return false;
+  }
+  
+}; // namespace coil
