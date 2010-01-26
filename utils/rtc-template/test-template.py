998b635daf379e1159f0dce1b761f2e9452e2a0b
n-ando <Noriaki.Ando@gmail.com>
2010-01-26 02:24:49 +0000

    test-template.py has been updated for 1.0.0 rtm/StringUtil.h -> coil/stringutil.h, split() and stringTo()
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@1789 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- utils/rtc-template/test-template.py
+++ utils/rtc-template/test-template.py
@@ -198,20 +198,20 @@ vector_convert_h = """\
 #include <ostream>
 #include <vector>
 #include <string>
-#include <rtm/StringUtil.h>
+#include <coil/stringutil.h>
 
 template<typename T>
 std::istream& operator>>(std::istream& is, std::vector<T>& v)
 {
   std::string s;
-  std::vector<std::string> sv;
+  coil::vstring sv;
   is >> s;
-  sv = split(s ,",");
+  sv = coil::split(s ,",");
   v.resize(sv.size());
   for (int i(0), len(sv.size()); i < len; ++i)
     {
       T tv;
-      if (stringTo(tv, sv[i].c_str()))
+      if (coil::stringTo(tv, sv[i].c_str()))
         {
           v[i] = tv;
         }
