7d3c744c2f4c871b61981cfd461426e496743b10
n-ando <Noriaki.Ando@gmail.com>
2010-02-08 07:00:15 +0000

    doxygen documentation has been updated. refs 1123
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_0@1853 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/PublisherBase.h
+++ src/lib/rtm/PublisherBase.h
@@ -5,7 +5,7 @@
  * @date $Date: 2007-12-31 03:08:06 $
  * @author Noriaki Ando <n-ando@aist.go.jp>
  *
- * Copyright (C) 2006-2008
+ * Copyright (C) 2006-2010
  *     Noriaki Ando
  *     Task-intelligence Research Group,
  *     Intelligent Systems Research Institute,
@@ -106,50 +106,151 @@ namespace RTC
      * @endif
      */
     virtual ReturnCode init(coil::Properties& prop) = 0;
+
     /*!
      * @if jp
      * @brief InPortコンシューマのセット
-     * @param consumer InPortコンシューマ
+     *
+     * この関数では、この Publisher に関連付けられるコンシューマをセットする。
+     * コンシューマオブジェクトがヌルポインタの場合、INVALID_ARGSが返される。
+     * それ以外の場合は、PORT_OK が返される。
+     *
+     * @param consumer Consumer へのポインタ
+     * @return ReturnCode PORT_OK 正常終了
+     *                    INVALID_ARGS 引数に不正な値が含まれている
+     *
      * @else
      * @brief Store InPort consumer
-     * @param consumer InPort consumer
+     *
+     * This operation sets a consumer that is associated with this
+     * object. If the consumer object is NULL, INVALID_ARGS will be
+     * returned.
+     *
+     * @param consumer A pointer to a consumer object.
+     * @return ReturnCode PORT_OK normal return
+     *                    INVALID_ARGS given argument has invalid value
+     *
      * @endif
      */
     virtual ReturnCode setConsumer(InPortConsumer* consumer) = 0;
+
     /*!
      * @if jp
      * @brief バッファのセット
-     * @param buffer CDRバッファ
+     *
+     * この関数では、この Publisher に関連付けられるバッファをセットする。
+     * バッファオブジェクトがヌルポインタの場合、INVALID_ARGSが返される。
+     * それ以外の場合は、PORT_OK が返される。
+     *
+     * @param buffer CDR buffer へのポインタ
+     * @return ReturnCode PORT_OK 正常終了
+     *                    INVALID_ARGS 引数に不正な値が含まれている
+     *
      * @else
      * @brief Setting buffer pointer
-     * @param buffer CDR buffer
+     *
+     * This operation sets a buffer that is associated with this
+     * object. If the buffer object is NULL, INVALID_ARGS will be
+     * returned.
+     *
+     * @param buffer A pointer to a CDR buffer object.
+     * @return ReturnCode PORT_OK normal return
+     *                    INVALID_ARGS given argument has invalid value
+     *
      * @endif
      */
     virtual ReturnCode setBuffer(BufferBase<cdrMemoryStream>* buffer) = 0;
+
     /*!
      * @if jp
      * @brief リスナを設定する。
-     * @param info ConnectorInfo
-     * @param listeners ConnectorListeners 
+     *
+     * Publisher に対してリスナオブジェクト ConnectorListeners を設定する。
+     * 各種リスナオブジェクトを含む ConnectorListeners をセットすることで、
+     * バッファの読み書き、データの送信時等にこれらのリスナをコールする。
+     * ConnectorListeners オブジェクトの所有権はポートまたは RTObject が持ち
+     * Publisher 削除時に ConnectorListeners は削除されることはない。
+     * ConnectorListeners がヌルポインタの場合 INVALID_ARGS を返す。
+     *
+     * @param info ConnectorProfile をローカル化したオブジェクト ConnectorInfo
+     * @param listeners リスナを多数保持する ConnectorListeners オブジェクト
+     * @return PORT_OK      正常終了
+     *         INVALID_ARGS 不正な引数
      * @else
      * @brief Set the listener. 
-     * @param info ConnectorInfo
-     * @param listeners ConnectorListeners 
+     *
+     * This function sets ConnectorListeners listener object to the
+     * Publisher. By setting ConnectorListeners containing various
+     * listeners objects, these listeners are called at the time of
+     * reading and writing of a buffer, and transmission of data
+     * etc. Since the ownership of the ConnectorListeners object is
+     * owned by Port or RTObject, the Publisher never deletes the
+     * ConnectorListeners object. If the given ConnectorListeners'
+     * pointer is NULL, this function returns INVALID_ARGS.
+     *
+     * @param info ConnectorInfo that is localized object of ConnectorProfile
+     * @param listeners ConnectorListeners that holds various listeners
+     * @return PORT_OK      Normal return
+     *         INVALID_ARGS Invalid arguments
      * @endif
      */
     virtual ReturnCode setListener(ConnectorInfo& info,
                                    ConnectorListeners* listeners) = 0;
+
     /*!
      * @if jp
      * @brief データを書き込む
+     *
+     * Publisher に対してデータを書き込む。コンシューマ、リスナ等が適切
+     * に設定されていない等、Publisher オブジェクトが正しく初期化されて
+     * いない場合、この関数を呼び出すとエラーコード
+     * PRECONDITION_NOT_MET が返され、書き込み等の操作は一切行われない。
+     *
+     * 書き込みに対して、コンシューマがフル状態、コンシューマのエラー、
+     * コンシューマへの書き込みがタイムアウトした場合にはそれぞれ、エラー
+     * コード SEND_FULL, SEND_ERROR, SEND_TIMEOUT が返される。
+     *
+     * これら以外のエラーの場合、PORT_ERROR が返される。
+     * 
+     *
      * @param data 書き込むデータ 
      * @param sec タイムアウト時間
      * @param nsec タイムアウト時間
+     *
+     * @return PORT_OK             正常終了
+     *         PRECONDITION_NO_MET consumer, buffer, listener等が適切に設定
+     *                             されていない等、このオブジェクトの事前条件
+     *                             を満たさない場合。
+     *         SEND_FULL           送信先がフル状態
+     *         SEND_TIMEOUT        送信先がタイムアウトした
+     *         CONNECTION_LOST     接続が切断されたことを検知した。
+     *
      * @else
      * @brief Write data 
-     * @param data Data
-     * @param sec Timeout period
-     * @param nsec Timeout period
+     *
+     * This function writes data into the Publisher. If this function
+     * is called without initializing correctly such as a consumer,
+     * listeners, etc., error code PRECONDITION_NOT_MET will be
+     * returned and no operation of the writing etc. will be
+     * performed.
+     *
+     * When publisher writes data, if the consumer returns
+     * full-status, returns error, is returned with timeout, error
+     * codes BUFFER_FULL, BUFFER_ERROR and BUFFER_TIMEOUT will be
+     * returned respectively.
+     *
+     * In other cases, PROT_ERROR will be returned.
+     *
+     * @param data Data to be wrote to the buffer
+     * @param sec Timeout time in unit seconds
+     * @param nsec Timeout time in unit nano-seconds
+     * @return PORT_OK             Normal return
+     *         PRECONDITION_NO_MET Precondition does not met. A consumer,
+     *                             a buffer, listenes are not set properly.
+     *         SEND_FULL           Data was sent but full-status returned
+     *         SEND_TIMEOUT        Data was sent but timeout occurred
+     *         CONNECTION_LOST     detected that the connection has been lost
+     *
      * @endif
      */
     virtual ReturnCode write(const cdrMemoryStream& data,
@@ -161,15 +262,22 @@ namespace RTC
      *
      * @brief アクティブ化確認
      * 
-     * アクティブ化されているか確認する。
+     * Publisher はデータポートと同期して activate/deactivate される。
+     * activate() / deactivate() 関数によって、アクティブ状態と非アクティ
+     * ブ状態が切り替わる。この関数により、現在アクティブ状態か、非アク
+     * ティブ状態かを確認することができる。
      *
      * @return 状態確認結果(アクティブ状態:true、非アクティブ状態:false)
      *
      * @else
      *
-     * @brief Confirm to activate
+     * @brief If publisher is active state
      * 
-     * Confirm that has been activated.
+     * A Publisher can be activated/deactivated synchronized with the
+     * data port.  The active state and the non-active state are made
+     * transition by the "activate()" and the "deactivate()" functions
+     * respectively. This function confirms if the publisher is in
+     * active state.
      *
      * @return Result of state confirmation
      *         (Active state:true, Inactive state:false)
@@ -177,32 +285,61 @@ namespace RTC
      * @endif
      */
     virtual bool isActive() = 0;
+
     /*!
      * @if jp
-     * @brief アクティブ化
+     * @brief アクティブ化する
+     *
+     * Publisher をアクティブ化する。この関数を呼び出すことにより、
+     * Publisherが持つ、データを送信するスレッドが動作を開始する。初期
+     * 化が行われていないなどにより、事前条件を満たさない場合、エラーコー
+     * ド PRECONDITION_NOT_MET を返す。
+     *
+     * @return PORT_OK 正常終了
+     *         PRECONDITION_NOT_MET 事前条件を満たさない
      *
      * @else
-     *
      * @brief activation
+     *
+     * This function activates the publisher. By calling this
+     * function, this publisher starts the thread that pushes data to
+     * InPort. If precondition such as initialization process and so
+     * on is not met, the error code PRECONDITION_NOT_MET is returned.
+     *
+     * @return PORT_OK normal return
+     *         PRECONDITION_NOT_MET precondition is not met
      *
      * @endif
      */
     virtual ReturnCode activate() = 0;
+
     /*!
      * @if jp
-     * @brief 非アクティブ化
+     * @brief 非アクティブ化する
+     *
+     * Publisher を非アクティブ化する。この関数を呼び出すことにより、
+     * Publisherが持つ、データを送信するスレッドが動作を停止する。初期
+     * 化が行われていないなどにより、事前条件を満たさない場合、エラーコー
+     * ド PRECONDITION_NOT_MET を返す。
+     *
+     * @return PORT_OK 正常終了
+     *         PRECONDITION_NOT_MET 事前条件を満たさない
      *
      * @else
-     *
      * @brief deactivation
+     *
+     * This function deactivates the publisher. By calling this
+     * function, this publisher stops the thread that pushes data to
+     * InPort. If precondition such as initialization process and so
+     * on is not met, the error code PRECONDITION_NOT_MET is returned.
+     *
+     * @return PORT_OK normal return
+     *         PRECONDITION_NOT_MET precondition is not met
      *
      * @endif
      */
     virtual ReturnCode deactivate() = 0;
-
-
-    
-
+  
     /*!
      * @if jp
      *
