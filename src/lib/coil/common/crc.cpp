009881dd0dfb078f6c3d64c21f3e6a21a8f823d5
hakuta <openrtm@openrtm.org>
2010-03-02 04:04:48 +0000

    coil documentation has been updated.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_0@1925 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/coil/common/crc.cpp
+++ src/lib/coil/common/crc.cpp
@@ -20,15 +20,29 @@
 namespace coil
 {
   /*
-   * @brief crc16 calculation functionSh
+   * @if jp
+   * @brief CRC-16 計算関数
+   *
+   * CRC種類: CRC-CCITT
+   * CRC多項式:  x^16 + x^12 + x^5 + 1 (0x1021)
+   * 初期値:  0xFFFF
+   * 出力XOR: 0x0000
+   * 入力ビット反転: なし
+   * 出力ビット反転: なし
+   * ビットシフト: 左
+   *
+   * @else
+   * @brief CRC-16 calculation function
    *
    * CRC type: CRC-CCITT
    * CRC generator polynomial:  x^16 + x^12 + x^5 + 1 (0x1021)
-   * Initial value:: 0xFFFF
+   * Initial value: 0xFFFF
    * Output XOR: 0x0000
    * Input bit inversion: None
    * Output bit inversion: None
    * Bit shift: left
+   *
+   * @endif
    */
   unsigned short crc16(const char* str, size_t len)
   {
@@ -79,18 +93,31 @@ namespace coil
   }
   
   /*!
-   * @brief crc32 calculation function
+   * @if jp
+   * @brief CRC-32 計算関数
+   *
+   * CRC種類: RFC2083 Appendix 15
+   * CRC多項式:  0xedb88320L
+   * 初期値:  0xFFFFFFFF
+   * 出力XOR: 0xFFFFFFFF
+   * 入力ビット反転: なし
+   * 出力ビット反転: なし
+   * ビットシフト: 右
+   *
+   * @else
+   * @brief CRC-32 calculation function
    * 
    * CRC type: RFC2083 Appendix 15
    *          http://www.faqs.org/rfcs/rfc2083.html
    *          http://www.efg2.com/Lab/Mathematics/CRC.htm
    * CRC polynomial:  0xedb88320L
-   * Initial value:: 0xFFFFFFFF
+   * Initial value: 0xFFFFFFFF
    * Output XOR: 0xFFFFFFFF
    * Input bit inversion: None
    * Output bit inversion: None
    * Bit shift: right
    * 
+   * @endif
    */
   unsigned long crc32(const char* str, size_t len)
   {
