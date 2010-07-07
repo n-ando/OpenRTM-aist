691a6bddcc75b4841b33777af443eae352cfde47
fsi-katami <openrtm@openrtm.org>
2010-07-07 04:54:44 +0000

    The unittest program has been updated.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_0@1985 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/tests/Manager/DummyModule2.cpp
+++ src/lib/rtm/tests/Manager/DummyModule2.cpp
@@ -1,3 +1,4 @@
+#include <rtm/DataFlowComponentBase.h>
 #include "DummyModule2.h"
 
 namespace RTC
@@ -6,8 +7,13 @@ namespace RTC
 class Manager;
 
 class DummyModule2
+  : public RTC::DataFlowComponentBase
 {
 public:
+        DummyModule2(RTC::Manager* manager)
+            : RTC::DataFlowComponentBase(manager)
+        {
+        };
 	static void InitProc(Manager* manager) { m_counter++; }
 	static int getInitProcCount() { return m_counter; }
 	static void resetInitProcCount() { m_counter = 0; }
@@ -32,4 +38,32 @@ void resetInitProcCount()
 	DummyModule2::resetInitProcCount();
 }
 
+};
+
+static const char* dummy2_spec[] =
+  {
+    "implementation_id", "Dummy2",
+    "type_name",         "Dummy2",
+    "description",       "",
+    "version",           "",
+    "vendor",            "",
+    "category",          "",
+    "activity_type",     "",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+
+extern "C"
+{
+ 
+  void DummyModule2Init(RTC::Manager* manager)
+  {
+    RTC::Properties profile(dummy2_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<RTC::DummyModule2>,
+                             RTC::Delete<RTC::DummyModule2>);
+  }
+  
 };
