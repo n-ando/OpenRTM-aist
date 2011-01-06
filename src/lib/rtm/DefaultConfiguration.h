4abd676b783c65206695b5ec3284783b4a0f11b2
n-ando <Noriaki.Ando@gmail.com>
2011-01-06 17:07:35 +0000

    The default configuration has been changed for new logger millisec/microsec timestamp feature.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2020 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/DefaultConfiguration.h
+++ src/lib/rtm/DefaultConfiguration.h
@@ -52,7 +52,7 @@ static const char* default_config[] =
     "os.hostname",                        "",
     "logger.enable",                      "YES",
     "logger.file_name",                   "./rtc%p.log",
-    "logger.date_format",                 "%b %d %H:%M:%S",
+    "logger.date_format",                 "%b %d %H:%M:%S.%Q",
     "logger.log_level",                   "INFO",
     "logger.stream_lock",                 "NO",
     "logger.master_logger",               "",
