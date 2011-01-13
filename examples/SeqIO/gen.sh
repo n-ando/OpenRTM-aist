e8672eb09adb084030b75fc9c55591b6d8e62e7e
fsi-katami <openrtm@openrtm.org>
2011-01-13 10:50:20 +0000

    Added Octet-type to SeqIO.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_0@2024 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SeqIO/gen.sh
+++ examples/SeqIO/gen.sh
@@ -7,10 +7,12 @@ rtc-template -bcxx \
     --module-category=example \
     --module-comp-type=DataFlowComponent --module-act-type=SPORADIC \
     --module-max-inst=10 \
+    --inport=Octet:TimedOctet \
     --inport=Short:TimedShort \
     --inport=Long:TimedLong \
     --inport=Float:TimedFloat \
     --inport=Double:TimedDouble \
+    --inport=OctetSeq:TimedOctetSeq \
     --inport=ShortSeq:TimedShortSeq \
     --inport=LongSeq:TimedLongSeq \
     --inport=FloatSeq:TimedFloatSeq \
@@ -24,10 +26,12 @@ rtc-template -bcxx \
     --module-category=example \
     --module-comp-type=DataFlowComponent --module-act-type=SPORADIC \
     --module-max-inst=10 \
+    --outport=Octet:TimedOctet \
     --outport=Short:TimedShort \
     --outport=Long:TimedLong \
     --outport=Float:TimedFloat \
     --outport=Double:TimedDouble \
+    --outport=OctetSeq:TimedOctetSeq \
     --outport=ShortSeq:TimedShortSeq \
     --outport=LongSeq:TimedLongSeq \
     --outport=FloatSeq:TimedFloatSeq \
