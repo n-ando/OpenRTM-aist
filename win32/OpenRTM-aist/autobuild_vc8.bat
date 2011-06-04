d3b93dbc314c790ef1dc3cb58d71a7cb1f6a4450
n-ando <Noriaki.Ando@gmail.com>
2011-06-04 08:26:56 +0000

    OpenRTMConfig.cmake has been introduced for UNIX and Windows. Some autobuidl batchfiles for Win32 now create OpenRTMConfig.cmake. OpenRTMConfig.cmake is installed into %RTM_ROOT%/cmake in Windows and prefix/openrtm-1.1/cmake in UNIX.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2164 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- win32/OpenRTM-aist/autobuild_vc8.bat
+++ win32/OpenRTM-aist/autobuild_vc8.bat
@@ -35,14 +35,18 @@ copy   etc\rtm_config_omni414.vsprops rtm_config.vsprops
 @rem ============================================================
 @rem convert property sheet to cmake
 @rem ============================================================
-if not exist rtm_config.cmake (
+set TMP_PYTHONPATH=%PYTHONPATH%
+set PYTHONPATH=./bin;%PYTHONPATH%
 
-   set TMP_PYTHONPATH=%PYTHONPATH%
-   set PYTHONPATH=./bin;%PYTHONPATH%
-   echo Generating cmake file
-   build\vsprops2cmake.py rtm_config.vsprops
-   set PYTHONPATH=%TMP_PYTHONPATH%
-)
+echo Generating rtm_config.cmake file
+build\vsprops2cmake.py rtm_config.vsprops
+move rtm_config.cmake cmake
+
+echo Generating OpenRTMConfig.cmake file
+build\cmakeconfgen.py rtm_config.vsprops
+move OpenRTMConfig.cmake cmake
+
+set PYTHONPATH=%TMP_PYTHONPATH%
 
 @rem ============================================================
 @rem build OpenRTM-aist
