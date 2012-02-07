2ed9ddf9b30354d1eb299120574dbc07a13ef5d2
n-ando <Noriaki.Ando@gmail.com>
2012-02-07 02:09:55 +0000

    [incompat,impl/header,func] ClockManager class has been introduced. refs #2355
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@2310 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/coil/common/ClockManager.cpp
@@ -0,0 +1,114 @@
+// -*- C++ -*-
+/*!
+ * @file ClockManager.cpp
+ * @brief Global clock management class
+ * @date $Date$
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2012
+ *     Noriaki Ando
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id$
+ *
+ */
+
+#include <coil/Time.h>
+#include <math.h>
+#include <coil/ClockManager.h>
+
+namespace coil
+{
+  //============================================================
+  // System Clock
+  //============================================================
+  SystemClock::~SystemClock()
+  {
+  }
+  coil::TimeValue SystemClock::gettime() const
+  {
+    return coil::gettimeofday();
+  }
+  bool SystemClock::settime(coil::TimeValue clocktime)
+  {
+    timeval tv;
+    tv.tv_sec = clocktime.sec();
+    tv.tv_usec = clocktime.usec();
+    if (coil::settimeofday(&tv, 0) == 0)
+      {
+        return true;
+      }
+    return false;
+  }
+  //
+  //============================================================
+
+  //============================================================
+  // Logical Time Clock
+  //============================================================
+  LogicalClock::LogicalClock()
+    : m_currentTime(0.0)
+  {
+  }
+
+  LogicalClock::~LogicalClock()
+  {
+  }
+
+  coil::TimeValue LogicalClock::gettime() const
+  {
+    return m_currentTime;
+  }
+
+  bool LogicalClock::settime(coil::TimeValue clocktime)
+  {
+    m_currentTime = clocktime;
+    return true;
+  }
+  //
+  //============================================================
+
+  //============================================================
+  // Adjusted Clock
+  //============================================================
+  AdjustedClock::AdjustedClock()
+    : m_offset(0.0)
+  {
+  }
+  
+  AdjustedClock::~AdjustedClock()
+  {
+  }
+  
+  coil::TimeValue AdjustedClock::gettime() const
+  {
+    return coil::gettimeofday() - m_offset;
+  }
+  
+  bool AdjustedClock::settime(coil::TimeValue clocktime)
+    {
+      m_offset = coil::gettimeofday() - clocktime;
+      return true;
+    }
+
+  IClock& ClockManager::getClock(std::string clocktype)
+  {
+    if (clocktype == "logical")
+      {
+        return m_logicalClock;
+      }
+    else if (clocktype == "adjusted")
+      {
+        return m_adjustedClock;
+      }
+    else if (clocktype == "system")
+      {
+        return m_systemClock;
+      }
+    return m_systemClock;
+  }
+
+}; // namespace coil
