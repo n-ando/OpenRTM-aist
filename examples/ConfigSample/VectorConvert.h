1e1998f67c82551bf70dffb6ea4075640143b396
n-ando <Noriaki.Ando@gmail.com>
2014-02-06 03:20:51 +0000

    [merge] r2411-2412 has been merged from trunk.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_1@2413 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/ConfigSample/VectorConvert.h
+++ examples/ConfigSample/VectorConvert.h
@@ -6,21 +6,25 @@
 #include <string>
 #include <coil/stringutil.h>
 
-template<typename T>
-std::istream& operator>>(std::istream& is, std::vector<T>& v)
+namespace std
 {
-  std::string s;
-  std::vector<std::string> sv;
-  is >> s;
-  sv = coil::split(s ,",");
-  v.resize(sv.size());
-  for (int i(0), len(sv.size()); i < len; ++i)
-    {
-      T tv;
-      if (coil::stringTo(tv, sv[i].c_str()))
-	{
-	  v[i] = tv;
-	}
-    }
-  return is;
+  template<typename T>
+  std::istream& operator>>(std::istream& is, std::vector<T>& v)
+  {
+    std::string s;
+    std::vector<std::string> sv;
+    is >> s;
+    sv = coil::split(s ,",");
+    v.resize(sv.size());
+    for (int i(0), len(sv.size()); i < len; ++i)
+      {
+	T tv;
+	if (coil::stringTo(tv, sv[i].c_str()))
+	  {
+	    v[i] = tv;
+	  }
+      }
+    return is;
+  }
 }
+
