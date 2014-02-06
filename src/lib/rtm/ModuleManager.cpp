b82b34023737844a80d0484520dfc5590649bbd7
n-ando <Noriaki.Ando@gmail.com>
2014-02-06 03:57:04 +0000

    [compat,bugfix] coil::flatten() in RELENG_1_1 has no second argument.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_1@2414 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/ModuleManager.cpp
+++ src/lib/rtm/ModuleManager.cpp
@@ -258,7 +258,7 @@ namespace RTC
    */
   void ModuleManager::setLoadpath(const std::vector<std::string>& load_path)
   {
-    RTC_TRACE(("setLoadpath(%s)", coil::flatten(load_path, ", ").c_str()));
+    RTC_TRACE(("setLoadpath(%s)", coil::flatten(load_path).c_str()));
     m_loadPath = load_path;
     return;
   }
@@ -272,7 +272,7 @@ namespace RTC
    */
   void ModuleManager::addLoadpath(const std::vector<std::string>& load_path)
   {
-    RTC_TRACE(("addLoadpath(%s)", coil::flatten(load_path, ", ").c_str()));
+    RTC_TRACE(("addLoadpath(%s)", coil::flatten(load_path).c_str()));
     StringVectorConstItr it(load_path.begin());
     StringVectorConstItr it_end(load_path.end());
     
@@ -354,7 +354,7 @@ namespace RTC
 				      const std::vector<std::string>& load_path)
   {
     RTC_TRACE(("findFile(%s, %s)", fname.c_str(),
-	       coil::flatten(load_path, ", ").c_str()));
+	       coil::flatten(load_path).c_str()));
     StringVectorConstItr it, it_end;
     std::string file_name(fname);
     
