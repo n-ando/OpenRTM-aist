c8dc6d29300855a729a79008f515633e2203d214
win-ei <openrtm@openrtm.org>
2014-02-12 08:50:05 +0000

    These files are due to be generated by omniidl_be.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/work_ForDoil@2458 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/doil/utils/omniidl_be/tests/ExecutionContextServiceProxy.cpp
@@ -0,0 +1,84 @@
+// -*- C++ -*-
+/*!
+ * @file ExecutionContextServiceProxy.cpp 
+ * @brief IExecutionContextService CORBA proxy for doil
+ * @date $Date$
+ * @author This file was automatically generated form RTC.idl 
+ *         by omniidl/doil backend
+ *
+ * $Id$
+ */
+
+#include <doil/corba/CORBAManager.h>
+#include <IExecutionContextService.h>
+#include <ExecutionContextServiceProxy.h>
+#include <RTCTypeConversion.h>
+#include <doil/corba/BasicTypeConversion.h>
+
+namespace RTC 
+{
+namespace CORBA 
+{
+  /*!
+   * @brief ctor
+   */ 
+  ExecutionContextServiceProxy::ExecutionContextServiceProxy(::CORBA::Object_ptr obj)
+   : ::doil::CORBA::CORBAProxyBase(obj),
+     m_obj(::RTC::ExecutionContextService::_nil()),
+     m_refcount(1),
+     ::RTC::CORBA::ExecutionContextProxy(obj),
+     ::SDOPackage::CORBA::SDOServiceProxy(obj)//   : m_obj(::RTC::ExecutionContextService::_nil()),
+//     ::RTC::ExecutionContextProxy(obj),
+//     ::SDOPackage::SDOServiceProxy(obj)
+  {
+    m_obj = ::RTC::ExecutionContextService::_narrow(obj);
+    if (::CORBA::is_nil(m_obj)) throw std::bad_alloc();
+    m_obj = ::RTC::ExecutionContextService::_duplicate(m_obj);
+  }
+
+  /*!
+   * @brief dtor
+   */ 
+  ExecutionContextServiceProxy::~ExecutionContextServiceProxy()
+  {
+    ::CORBA::release(m_obj);
+  }
+
+  
+  /*!
+   * @brief get_profile 
+   */ 
+  ::RTC::Local::ExecutionContextProfile ExecutionContextServiceProxy::get_profile()
+      throw ()
+  {
+    // Convert Local to CORBA.
+    // (The direction of the argument is 'in' or 'inout'.)
+
+    // Execute the method. 
+    ::RTC::ExecutionContextProfile* corba_ret;
+    ::RTC::Local::ExecutionContextProfile local_ret;
+    corba_ret = m_obj->get_profile();
+
+    // Convert CORBA to Local.
+    // (The direction of the argument is 'out' or 'inout'.)
+
+    // Generate the return value.
+    corba_to_local(*corba_ret, local_ret);
+    delete corba_ret; 
+    return local_ret;
+  }
+
+}; // namespace CORBA
+}; // namespace RTC 
+
+extern "C"
+{
+  void ExecutionContextServiceProxyCORBAInit(coil::Properties& prop)
+  {
+    doil::CORBA::CORBAManager& mgr(doil::CORBA::CORBAManager::instance());
+    mgr.registerProxyFactory("ExecutionContextServiceProxy",
+                        doil::New< ::RTC::CORBA::ExecutionContextServiceProxy >,
+                        doil::Delete< ::RTC::CORBA::ExecutionContextServiceProxy >);
+  }
+};
+
