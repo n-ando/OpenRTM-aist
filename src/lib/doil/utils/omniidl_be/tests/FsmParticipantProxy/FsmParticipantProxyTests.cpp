d45f7b76b6d83d4fdc6457cfdebcf1201e43bae2
win-ei <openrtm@openrtm.org>
2014-02-12 13:33:43 +0000

    Added the test code of Proxy.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/work_ForDoil@2465 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/doil/utils/omniidl_be/tests/FsmParticipantProxy/FsmParticipantProxyTests.cpp
@@ -0,0 +1,167 @@
+// -*- C++ -*-
+/*!
+ * @file   FsmParticipantProxyTests.cpp
+ * @brief  FsmParticipantProxy test class
+ * @date   $Date$ 
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * $Id$ 
+ *
+ */
+
+/*
+ * $Log$
+ *
+ */
+
+#ifndef FsmParticipantProxy_cpp
+#define FsmParticipantProxy_cpp
+
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestAssert.h>
+#include <string>
+#include <iostream>
+#include <idl/SDOPackageSkel.h>
+#include <idl/RTCSkel.h>
+#include <FsmParticipantProxy.h>
+#include <rtm/NVUtil.h>
+#include <ISDOService.h>
+#include <ISDOSystemElement.h>
+#include <IOrganization.h>
+#include <doil/corba/CORBAManager.h>
+
+
+/*!
+ * @class FsmParticipantProxyTests class
+ * @brief FsmParticipantProxy test
+ */
+namespace FsmParticipantProxy
+{
+  class Logger
+  {
+  public:
+    void log(const std::string& msg)
+    {
+      m_log.push_back(msg);
+    }
+
+    int countLog(const std::string& msg)
+    {
+      int count = 0;
+      for (int i = 0; i < (int) m_log.size(); ++i)
+        {
+          if (m_log[i] == msg) ++count;
+        }
+     return count;
+    }
+		
+  private:
+    std::vector<std::string> m_log;
+  };
+
+  /*!
+   * 
+   * 
+   *
+   */
+  class FsmParticipantMock
+    : public virtual ::POA_RTC::FsmParticipant
+  {
+  protected:
+      ::std::vector<std::string> m_log;
+  private:
+  public :
+      FsmParticipantMock(){}
+      virtual ~FsmParticipantMock()
+      {
+      }
+
+      void setLogger(Logger* logger)
+      {
+        m_logger = logger;
+      }
+
+  private:
+    Logger* m_logger;
+
+  };
+
+  class FsmParticipantProxyTests
+   : public CppUnit::TestFixture
+  {
+    CPPUNIT_TEST_SUITE(FsmParticipantProxyTests);
+    CPPUNIT_TEST_SUITE_END();
+  
+  private:
+    CORBA::ORB_ptr m_pORB;
+    PortableServer::POA_ptr m_pPOA;
+  
+  public:
+  
+    /*!
+     * @brief Constructor
+     */
+    FsmParticipantProxyTests()
+    {
+      int argc = 0;
+      char** argv = NULL;
+      m_pORB = CORBA::ORB_init(argc, argv);
+      m_pPOA = PortableServer::POA::_narrow(
+                       m_pORB->resolve_initial_references("RootPOA"));
+      m_pPOA->the_POAManager()->activate();
+    }
+    
+    /*!
+     * @brief Destructor
+     */
+    ~FsmParticipantProxyTests()
+    {
+    }
+  
+    /*!
+     * @brief Test initialization
+     */
+    virtual void setUp()
+    {
+    }
+    
+    /*!
+     * @brief Test finalization
+     */
+    virtual void tearDown()
+    { 
+    }
+  
+    /*! 
+     *
+     *
+     *
+     */
+    /* test case */
+    void test_case0()
+    {
+    }
+  };
+}; // namespace FsmParticipantProxy
+
+/*
+ * Register test suite
+ */
+CPPUNIT_TEST_SUITE_REGISTRATION(FsmParticipantProxy::FsmParticipantProxyTests);
+
+#ifdef LOCAL_MAIN
+int main(int argc, char* argv[])
+{
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+    CppUnit::Outputter* outputter = 
+      new CppUnit::TextOutputter(&runner.result(), std::cout);
+    runner.setOutputter(outputter);
+    bool retcode = runner.run();
+    return !retcode;
+}
+#endif // MAIN
+#endif // FsmParticipantProxy_cpp
