2970406ac2c39dd4b064efc4dca92ce97d8adb49
win-ei <openrtm@openrtm.org>
2014-02-13 14:23:51 +0000

    Daily work.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/work_ForDoil@2487 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/doil/utils/omniidl_be/tests/unitTest/stubs/ComponentActionImpl.h
@@ -0,0 +1,82 @@
+// -*- C++ -*-
+/*!
+ * @file ComponentActionImpl.h
+ * @brief ComponentActionImpl C++ implementation sample for doil
+ * @date $Date$
+ * @author 
+ *
+ * $Id$
+ */
+
+#ifndef COMPONENTACTION_IMPL_H
+#define COMPONENTACTION_IMPL_H
+
+
+#include <doil/ImplBase.h>
+#include <IComponentAction.h>
+#include <Logger.h>
+
+namespace UnitTest
+{
+namespace Servant
+{
+
+  /*!
+   * @if jp
+   * @class ComponentActionImpl
+   * @brief ComponentActionServant試験用インプリメントクラス
+   * @else
+   * @class ComponentActionImpl
+   * @brief ComponentAction implementation class for ComponentActionServant' unittest.
+   * @endif
+   */
+
+  class ComponentActionImpl
+   : public virtual doil::ImplBase,
+     public virtual RTC::Local::IComponentAction
+
+  {
+  public:
+    ComponentActionImpl();
+    ComponentActionImpl(Logger& aLogger);
+
+    virtual ~ComponentActionImpl();
+
+    virtual ::RTC::Local::ReturnCode_t on_initialize()
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_finalize()
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_startup(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_shutdown(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_activated(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_deactivated(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_aborting(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_error(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+    virtual ::RTC::Local::ReturnCode_t on_reset(::RTC::Local::ExecutionContextHandle_t exec_handle)
+      throw ();
+
+  private:
+    static int count;
+    char m_name[32];
+    int refcount;
+    Logger *m_logger;
+  };
+
+}; // namespace Servant
+}; // namespace UnitTest
+
+#endif  // 
