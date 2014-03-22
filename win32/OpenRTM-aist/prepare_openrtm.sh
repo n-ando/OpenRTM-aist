f1d4c45cf81ac3c83bd354a3c2e2f018962fbbee
n-ando <Noriaki.Ando@gmail.com>
2014-03-22 00:31:26 +0000

    [compat,->RELENG_1_1] build scripts updated.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@2538 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- win32/OpenRTM-aist/prepare_openrtm.sh
+++ win32/OpenRTM-aist/prepare_openrtm.sh
@@ -9,11 +9,11 @@
 #
 # The following environment variables should be set.
 #
-# OMNI_VERSION (= 4.1.7, ...)
-# OMNITH_VER (= 3.4, ...)
-# PYTHON_DIR (= /cygdrive/c/Python27)
-# VC_VERSION  (= 9, 10, ...)
-# ARCH (= x86 or x64)
+export OMNI_VERSION=4.1.7
+export OMNITH_VER=3.4
+export PYTHON_DIR=/cygdrive/c/Python27
+export VC_VERSION=10
+export ARCH=x86
 #
 export PATH=/cygdrive/c/cygwin64/bin:$PATH
 echo `pwd`
@@ -59,6 +59,10 @@ check_env()
 
 unpack_omnibin()
 {
+    omnidir=`find ./ -maxdepth 1 -name 'omniORB*' -type d`
+    if test -d $omnidir ; then
+        rm -rf $omnidir
+    fi
     # getting omniORB-4.1.7.tar.bz2
     if test ! -f $OMNIORB_ZIP ; then
         wget $OMNIORB_URL
@@ -66,30 +70,14 @@ unpack_omnibin()
     unzip $OMNIORB_ZIP
 }
 
-unpack_openrtm()
+rename_omnidir()
 {
-    OPENRTM_ZIP=`ls OpenRTM-aist*.zip`
-    if test "x$OPENRTM_ZIP" = "x" ; then
-        echo "OpenRTM-aist ZIP file not found."
-        exit 1
-    fi
-    unzip $OPENRTM_ZIP
-}
-
-copy_props()
-{
-    OpenRTM-aist/etc/rtm_config_omniorb415.vcprops OpenRTM-aist/rtm_config.vcprops
-    OpenRTM-aist/etc/rtm_config_omniorb415.props   OpenRTM-aist/rtm_config.props
-
-    sed -i "s/415/$OMNI_SHORT_VER/g" OpenRTM-aist/rtm_config.vcprops
-    sed -i "s/34/$OMNITH_SHORT_VER/g" OpenRTM-aist/rtm_config.vcprops
-    sed -i "s/415/$OMNI_SHORT_VER/g" OpenRTM-aist/rtm_config.props
-    sed -i "s/34/$OMNITH_SHORT_VER/g" OpenRTM-aist/rtm_config.props
+    omnidir=`find ./ -maxdepth 1 -name 'omniORB*' -type d`
+    mv ${omnidir} omniORB
 }
 check_env
 unpack_omnibin
-unpack_openrtm
-
+rename_omnidir
 
 # end of ecript
 #==============================
