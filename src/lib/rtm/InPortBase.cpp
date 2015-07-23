3077b6f4d616380386f3a4864ccbb545c32dbd17
n-ando <Noriaki.Ando@gmail.com>
2015-07-23 00:10:33 +0000

    [compat,->RELENG_1_1] Now properties given by rtc.conf are merged to PortProfile.properties.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2652 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/InPortBase.cpp
+++ src/lib/rtm/InPortBase.cpp
@@ -96,7 +96,17 @@ namespace RTC
   void InPortBase::init(coil::Properties& prop)
   {
     RTC_TRACE(("init()"));
+    RTC_PARANOID(("given properties:"));
+    RTC_DEBUG_STR((prop));
+
+    // merge properties to PortProfile.properties
     m_properties << prop;
+    NVList nv;
+    NVUtil::copyFromProperties(nv, m_properties);
+    CORBA_SeqUtil::push_back_list(m_profile.properties, nv);
+    RTC_PARANOID(("updated properties:"));
+    RTC_DEBUG_STR((m_properties));
+
     if (m_singlebuffer)
       {
         RTC_DEBUG(("single buffer mode."));
