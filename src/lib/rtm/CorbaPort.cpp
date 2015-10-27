79b50cc1cbfd2ff21b1c8eb24633343c2f86f6ee
n-ando <Noriaki.Ando@gmail.com>
2015-10-27 07:23:35 +0000

    [compat,->RELENG_1_1] Now PortProfile.properties of CorbaPort includes given port porperties from component configuration files. refs #3263
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk/OpenRTM-aist@2673 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/CorbaPort.cpp
+++ src/lib/rtm/CorbaPort.cpp
@@ -64,7 +64,9 @@ namespace RTC
     RTC_DEBUG_STR((prop));
 
     m_properties << prop;
-
+    NVList nv;
+    NVUtil::copyFromProperties(nv, m_properties);
+    CORBA_SeqUtil::push_back_list(m_profile.properties, nv);
     RTC_PARANOID(("updated properties:"));
     RTC_DEBUG_STR((m_properties));
 
