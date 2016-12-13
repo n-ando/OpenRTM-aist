b6b8b4e25112247c4f1d732031f5f5991676285a
takahashi <3263ta@gmail.com>
2016-12-13 08:05:32 +0000

    removed unnecessary files in DEV_IQ_2016 branch
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/DEV_IQ_2016@2816 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- tags/RELEASE_1_1_0_RC3/OpenRTM-aist/win32/OpenRTM-aist/installer/crean.cmd
+++
@@ -1,9 +0,0 @@
-@rem
-@rem @brief install directory cleanup script
-@rem
-
-del *.mst
-del *.wixobj
-del *.wixpdb
-del *~
-del *_*.msi
--- tags/RELENG_0_4_RC1/OpenRTM-aist/docs/DevelopersGuide/icon/conv.sh
+++
@@ -1,1 +0,0 @@
-ls *png | awk '{gsub(".png",""); printf("bmeps -c %s.png %s.eps\n", $1, $1);}' |sh
