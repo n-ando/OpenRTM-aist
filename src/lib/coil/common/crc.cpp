7abc23ef0a6c2d112f62f36247369d8ebcf08095
n-ando <Noriaki.Ando@gmail.com>
2017-02-11 02:08:18 +0000

    [compat,->DEV_IQ_2016] LDRA rule "No cast for widening complex int expression (MR)." has been made a countermeasured. refs #3909
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/DEV_IQ_2016@2944 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/coil/common/crc.cpp
+++ src/lib/coil/common/crc.cpp
@@ -135,7 +135,7 @@ namespace coil
      *      crc32table[n] = c;
      *   }
      */
-    static const unsigned crc32tab[256] =
+    static const unsigned int crc32tab[256] =
       {
         0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
         0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
@@ -184,7 +184,7 @@ namespace coil
     unsigned long c(0xffffffffL);
     for (size_t i(0); i < len; ++i)
       {
-        c = crc32tab[(c ^ str[i]) & 0xff] ^ (c >> 8);
+        c = crc32tab[(0xff ^ str[i]) & 0xff] ^ (c >> 8);
       }
     return c ^ 0xffffffffL;
   }
