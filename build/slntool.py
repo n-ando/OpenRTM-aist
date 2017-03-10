1a1da9a4fb800643b3a02af89ffb160877a53234
kawauchi <openrtm@openrtm.org>
2017-03-10 05:32:39 +0000

    [compat,build,->RELENG_1_2] vc15 (vc2017) has been supported.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2959 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- build/slntool.py
+++ build/slntool.py
@@ -28,6 +28,7 @@ vcversions = {"VC8": {"sln": "9.00", "vc": "2005"},
               "VC11": {"sln": "12.00", "vc": "2012"},
               "VC12": {"sln": "13.00", "vc": "2013"},
               "VC14": {"sln": "14.00", "vc": "2015"},
+              "VC15": {"sln": "15.00", "vc": "2017"},
               }
 sln_template = """Microsoft Visual Studio Solution File, Format Version %s
 # Visual Studio %s
@@ -82,7 +83,7 @@ Usage:
   slntool.py --dep dep_file [--outfile outfile] vcproj_files...
 
 Options:
-    --vcversion: Visual C++'s version [VC8|VC9|VC10|VC11|VC12|VC14]
+    --vcversion: Visual C++'s version [VC8|VC9|VC10|VC11|VC12|VC14|VC15]
     --dep: dependency file
     --out or --output: output file name
 
@@ -118,6 +119,7 @@ def get_projinfo(fname,vcversion="VC8"):
               "VC11": {"guid":'^.*?<ProjectGuid>{(.*)}</ProjectGuid>',"name":'^.*<ProjectName>(.*)</ProjectName>'},
               "VC12": {"guid":'^.*?<ProjectGuid>{(.*)}</ProjectGuid>',"name":'^.*<ProjectName>(.*)</ProjectName>'},
               "VC14": {"guid":'^.*?<ProjectGuid>{(.*)}</ProjectGuid>',"name":'^.*<ProjectName>(.*)</ProjectName>'},
+              "VC15": {"guid":'^.*?<ProjectGuid>{(.*)}</ProjectGuid>',"name":'^.*<ProjectName>(.*)</ProjectName>'},
              }
     re_guid = re.compile(regexs[vcversion]["guid"])
     re_name = re.compile(regexs[vcversion]["name"])
