b45bdb924f5f0473c9f21678cf3036579df53076
Nobu19800 <openrtm@openrtm.org>
2017-03-15 09:27:11 +0000

    [compat,bugfix,->RELENG_1_2] Modified cmakeconfgen.py
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2973 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- build/cmakeconfgen.py
+++ build/cmakeconfgen.py
@@ -221,7 +221,7 @@ if __name__ == '__main__':
     omni_libs  = process_lib(dict["omni_lib"], "optimized")
     omni_libs += ";" + process_lib(dict["omni_libd"], "debug")
     rtm_libs   = process_lib(dict["rtm_lib"], "optimized")
-    rtm_libs  += ";" process_lib(dict["rtm_ext_lib"], "optimized")
+    rtm_libs  += ";" + process_lib(dict["rtm_ext_lib"], "optimized")
     rtm_libs  += ";" + process_lib(dict["rtm_libd"], "debug")
     rtm_libs  += ";" + process_lib(dict["rtm_ext_libd"], "debug")
 
