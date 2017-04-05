af7eac3fe3da7e0c95c718a31595394093ed9803
n-ando <Noriaki.Ando@gmail.com>
2017-04-05 08:46:15 +0000

    [incompat,newfunc,->RELENG_1_2] CPU affinity functionality in coil implemented.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@2978 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/coil/posix/coil/Affinity.h
@@ -0,0 +1,121 @@
+// -*- C++ -*-
+/*!
+ * @file Affinity.h
+ * @brief  Processor affinity operation class
+ * @date $Date$
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2016
+ *     Noriaki Ando
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef COIL_AFFINITY_H
+#define COIL_AFFINITY_H
+
+#include <string>
+#include <coil/Singleton.h>
+#include <coil/TimeValue.h>
+#include <coil/Mutex.h>
+#include <coil/Guard.h>
+
+namespace coil
+{
+  typedef std::vector<unsigned int> CpuMask;
+  /*!
+   * @if jp
+   * @brief プロセスのCPU affinityを取得する
+   * @prop cpu_mask 現在設定されている affinity が CPU IDの
+   *                std::vector<unsigned int> で返される。
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Getting current process CPU affinity
+   * @prop cpu_mask Current CPU affinity mask is returned as CPU ID in
+   *                std::vector<unsigned int>.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool getProcCpuAffinity(CpuMask& cpu_mask);
+
+  /*!
+   * @if jp
+   * @brief プロセスのCPU affinityを設定する
+   * @prop cpu_mask 設定する CPU affinity を CPU ID の
+   *                std::vector<unsigned int> リストで与える
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Setting process CPU affinity
+   * @prop cpu_mask CPU affinity mask to be set is given with CPU ID in
+   *                std::vector<unsigned int> list.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool setProcCpuAffinity(const CpuMask cpu_mask);
+
+  /*!
+   * @if jp
+   * @brief プロセスのCPU affinityを文字列で設定する
+   * @prop cpu_mask 設定する CPU affinity を CPU ID のカンマ区切り文字
+   *                列のリストで与える
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Setting process CPU affinity with string
+   * @prop cpu_mask CPU affinity mask to be set is given with comma
+   *                separated CPU ID string.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool setProcCpuAffinity(std::string cpu_mask);
+
+  /*!
+   * @if jp
+   * @brief スレッドのCPU affinityを取得する
+   * @prop cpu_mask 現在設定されている affinity が CPU IDの
+   *                std::vector<unsigned int> で返される。
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Getting current process CPU affinity
+   * @prop cpu_mask Current CPU affinity mask is returned as CPU ID in
+   *                std::vector<unsigned int>.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool getThreadCpuAffinity(CpuMask& cpu_mask);
+
+  /*!
+   * @if jp
+   * @brief スレッドのCPU affinityを設定する
+   * @prop cpu_mask 設定する CPU affinity を CPU ID の
+   *                std::vector<unsigned int> リストで与える
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Setting process CPU affinity
+   * @prop cpu_mask CPU affinity mask to be set is given with CPU ID in
+   *                std::vector<unsigned int> list.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool setThreadCpuAffinity(const CpuMask cpu_mask);
+
+  /*!
+   * @if jp
+   * @brief スレッドのCPU affinityを文字列で設定する
+   * @prop cpu_mask 設定する CPU affinity を CPU ID のカンマ区切り文字
+   *                列のリストで与える
+   * @return True: 成功、False: 失敗
+   * @else
+   * @brief Setting process CPU affinity with string
+   * @prop cpu_mask CPU affinity mask to be set is given with comma
+   *                separated CPU ID string.
+   * @return True: success, False: fail
+   * @endif
+   */
+  bool setThreadCpuAffinity(std::string mask);
+
+}; // namespace coil
+#endif // COIL_AFFINITY_H
