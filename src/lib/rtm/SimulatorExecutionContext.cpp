81234cee99435449b97ad20eb0f4e4ee46320e77
Nobu19800 <openrtm@openrtm.org>
2017-06-12 08:14:36 +0000

    [incompat,Choreonoid,->RELENG_1_2] add SimulatorExecutionContext
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@3000 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/rtm/SimulatorExecutionContext.cpp
@@ -0,0 +1,136 @@
+﻿#include "SimulatorExecutionContext.h"
+#include <rtm/ECFactory.h>
+
+#ifdef OPENRTM_VERSION110
+#else
+#include <rtm/RTObjectStateMachine.h>
+#endif
+
+
+
+namespace RTC
+{
+
+	SimulatorExecutionContext::SimulatorExecutionContext()
+		: RTC::OpenHRPExecutionContext()
+  {
+	  
+	  
+	
+  }
+
+
+  SimulatorExecutionContext::~SimulatorExecutionContext()
+  {
+
+  }
+
+
+
+  RTC::ReturnCode_t SimulatorExecutionContext::
+	  activate_component(RTC::LightweightRTObject_ptr comp)
+	  throw (CORBA::SystemException)
+  {
+	Guard guard(m_tickmutex);
+
+	RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
+
+	if (rtobj == NULL)
+	{
+		return RTC::BAD_PARAMETER;
+	}
+	if (!(rtobj->isCurrentState(RTC::INACTIVE_STATE)))
+	{
+		return RTC::PRECONDITION_NOT_MET;
+	}
+	m_syncActivation = false;
+
+	RTC::ReturnCode_t ret = ExecutionContextBase::activateComponent(comp);
+	invokeWorkerPreDo();
+	if ((rtobj->isCurrentState(RTC::ACTIVE_STATE)))
+	{
+		return RTC::RTC_OK;
+	}
+	return RTC::RTC_ERROR;
+
+  }
+
+
+  RTC::ReturnCode_t SimulatorExecutionContext::
+  deactivate_component(RTC::LightweightRTObject_ptr comp)
+    throw (CORBA::SystemException)
+  {
+	Guard guard(m_tickmutex);
+
+	RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
+
+	if (rtobj == NULL)
+	{
+		  return RTC::BAD_PARAMETER;
+	}
+	if (!(rtobj->isCurrentState(RTC::ACTIVE_STATE)))
+	{
+		  return RTC::PRECONDITION_NOT_MET;
+	}
+	m_syncDeactivation = false;
+	RTC::ReturnCode_t ret = ExecutionContextBase::deactivateComponent(comp);
+	invokeWorkerPreDo();
+	invokeWorkerDo();
+	invokeWorkerPostDo();
+
+	if ((rtobj->isCurrentState(RTC::INACTIVE_STATE)))
+	{
+		return RTC::RTC_OK;
+	}
+	return RTC::RTC_ERROR;
+
+  }
+
+  RTC::ReturnCode_t SimulatorExecutionContext::
+  reset_component(RTC::LightweightRTObject_ptr comp)
+    throw (CORBA::SystemException)
+  {
+	Guard guard(m_tickmutex);
+
+	RTC_impl::RTObjectStateMachine* rtobj = m_worker.findComponent(comp);
+
+	if (rtobj == NULL)
+	{
+		return RTC::BAD_PARAMETER;
+	}
+	if (!(rtobj->isCurrentState(RTC::ERROR_STATE)))
+	{
+		return RTC::PRECONDITION_NOT_MET;
+	}
+	m_syncReset = false;
+	RTC::ReturnCode_t ret = ExecutionContextBase::resetComponent(comp);
+	invokeWorkerPreDo();
+	invokeWorkerDo();
+	invokeWorkerPostDo();
+
+	if ((rtobj->isCurrentState(RTC::INACTIVE_STATE)))
+	{
+		return RTC::RTC_OK;
+	}
+	return RTC::RTC_ERROR;
+
+
+  }
+
+};
+
+
+
+
+extern "C"
+{
+  void SimulatorExecutionContextInit(RTC::Manager* manager)
+  {
+    RTC::ExecutionContextFactory::
+      instance().addFactory("SimulatorExecutionContext",
+                            ::coil::Creator< ::RTC::ExecutionContextBase,
+                            ::RTC::SimulatorExecutionContext>,
+                            ::coil::Destructor< ::RTC::ExecutionContextBase,
+                            ::RTC::SimulatorExecutionContext>);
+  }
+};
---
+++ src/lib/rtm/SimulatorExecutionContext.h
@@ -0,0 +1,50 @@
+﻿#ifndef TEST_EC_H
+#define TEST_EC_H
+
+
+
+#include <coil/Mutex.h>
+#include <coil/Guard.h>
+#include <rtm/RTC.h>
+#include <rtm/ExecutionContextBase.h>
+#include <rtm/OpenHRPExecutionContext.h>
+
+
+namespace RTC
+{
+  class SimulatorExecutionContext
+	  : public virtual RTC::OpenHRPExecutionContext
+  {
+	  typedef coil::Mutex Mutex;
+	  typedef coil::Guard<coil::Mutex> Guard;
+  public:
+    SimulatorExecutionContext();
+    virtual ~SimulatorExecutionContext(void);
+
+    virtual RTC::ReturnCode_t
+    activate_component(RTC::LightweightRTObject_ptr comp)
+      throw (CORBA::SystemException);
+    virtual RTC::ReturnCode_t
+    deactivate_component(RTC::LightweightRTObject_ptr comp)
+      throw (CORBA::SystemException);
+    virtual RTC::ReturnCode_t
+    reset_component(RTC::LightweightRTObject_ptr comp)
+      throw (CORBA::SystemException);
+
+  private:
+
+  };
+};
+
+
+
+#ifdef WIN32
+#pragma warning( default : 4290 )
+#endif
+
+extern "C"
+{
+  DLL_EXPORT void SimulatorExecutionContextInit(RTC::Manager* manager);
+};
+
+#endif
