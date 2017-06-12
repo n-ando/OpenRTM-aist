88d77f463fde855b6965553ffd6b3fe4eec25634
Nobu19800 <openrtm@openrtm.org>
2017-06-12 08:27:23 +0000

    [incompat,Choreonoid,->RELENG_1_2] add comment
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@3001 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/SimulatorExecutionContext.cpp
+++ src/lib/rtm/SimulatorExecutionContext.cpp
@@ -1,17 +1,40 @@
-﻿#include "SimulatorExecutionContext.h"
+﻿// -*- C++ -*-
+/*!
+ * @file SimulatorExecutionContext.cpp
+ * @brief SimulatorExecutionContext class
+ * @date $Date$
+ * @author Nobuhiko Miyamoto <n-miyamoto@aist.go.jp>
+ *
+ * Copyright (C) 20017
+ *     Nobuhiko Miyamoto
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ *
+ */
+
+
+#include "SimulatorExecutionContext.h"
 #include <rtm/ECFactory.h>
 
-#ifdef OPENRTM_VERSION110
-#else
 #include <rtm/RTObjectStateMachine.h>
-#endif
+
 
 
 
 namespace RTC
 {
 
-	SimulatorExecutionContext::SimulatorExecutionContext()
+  /*!
+   * @if jp
+   * @brief コンストラクタ
+   * @else
+   * @brief Constructor
+   * @endif
+   */
+  SimulatorExecutionContext::SimulatorExecutionContext()
 		: RTC::OpenHRPExecutionContext()
   {
 	  
@@ -19,14 +42,41 @@ namespace RTC
 	
   }
 
-
+  /*!
+   * @if jp
+   * @brief デストラクタ
+   * @else
+   * @brief Destructor 
+   * @endif
+   */
   SimulatorExecutionContext::~SimulatorExecutionContext()
   {
 
   }
 
 
-
+  /*!
+   * @if jp
+   * @brief RTコンポーネントをアクティブ化する
+   *
+   * 内部でinvokeWorkerPreDo関数を呼ぶため、即座に
+   * 状態を遷移させることができる。
+   * 現在tick実行中の場合は実行終了まで待つ
+   *
+   * @param comp アクティブ化対象RTコンポーネント
+   *
+   * @return ReturnCode_t 型のリターンコード
+   *
+   * @else
+   *
+   * @brief Activate an RT-component
+   *
+   * @param comp The target RT-Component for activation
+   *
+   * @return The return code of ReturnCode_t type
+   *
+   * @endif
+   */
   RTC::ReturnCode_t SimulatorExecutionContext::
 	  activate_component(RTC::LightweightRTObject_ptr comp)
 	  throw (CORBA::SystemException)
@@ -56,6 +106,28 @@ namespace RTC
   }
 
 
+  /*!
+   * @if jp
+   * @brief RTコンポーネントを非アクティブ化する
+   *
+   * 内部でinvokeWorkerPreDo関数を呼ぶため、即座に
+   * 状態を遷移させることができる。
+   * 現在tick実行中の場合は実行終了まで待つ
+   *
+   * @param comp 非アクティブ化対象RTコンポーネント
+   *
+   * @return ReturnCode_t 型のリターンコード
+   *
+   * @else
+   *
+   * @brief Activate an RT-component
+   *
+   * @param comp The target RT-Component for deactivate
+   *
+   * @return The return code of ReturnCode_t type
+   *
+   * @endif
+   */
   RTC::ReturnCode_t SimulatorExecutionContext::
   deactivate_component(RTC::LightweightRTObject_ptr comp)
     throw (CORBA::SystemException)
@@ -86,6 +158,29 @@ namespace RTC
 
   }
 
+
+  /*!
+   * @if jp
+   * @brief RTコンポーネントをリセットする
+   *
+   * 内部でinvokeWorkerPreDo関数を呼ぶため、即座に
+   * 状態を遷移させることができる。
+   * 現在tick実行中の場合は実行終了まで待つ
+   *
+   * @param comp リセット対象RTコンポーネント
+   *
+   * @return ReturnCode_t 型のリターンコード
+   *
+   * @else
+   *
+   * @brief Activate an RT-component
+   *
+   * @param comp The target RT-Component for reset
+   *
+   * @return The return code of ReturnCode_t type
+   *
+   * @endif
+   */
   RTC::ReturnCode_t SimulatorExecutionContext::
   reset_component(RTC::LightweightRTObject_ptr comp)
     throw (CORBA::SystemException)
@@ -126,6 +221,13 @@ extern "C"
 {
   void SimulatorExecutionContextInit(RTC::Manager* manager)
   {
+  /*!
+   * @if jp
+   * @brief ECFactoryへの登録のための初期化関数
+   * @else
+   * @brief Initialization function to register to ECFactory
+   * @endif
+   */
     RTC::ExecutionContextFactory::
       instance().addFactory("SimulatorExecutionContext",
                             ::coil::Creator< ::RTC::ExecutionContextBase,
