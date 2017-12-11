e5a85dc8ff26bf7dd9bb5a26a243482a9eae81a2
n-ando <Noriaki.Ando@gmail.com>
2017-12-11 03:27:45 +0000

    merged changes from branches/FSM4RTC/OpenRTM-aist r2894, 2897, 2901: [compat,->RELENG_1_2,->RELENG_2_0] NVUtil dump() function outputs doubled CR. It is removed. [FSM4RTC,->RELENG_1_2,->RELENG_2_0] Some sample components listener have been modified to follow ConnectorListener changes. refs #3871 [coil,test,->RELENG_1_2,->RELENG_2_0] coil::stringutils tests added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_2@3125 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- examples/SeqIO/SeqOut.h
+++ examples/SeqIO/SeqOut.h
@@ -35,6 +35,7 @@ extern bool g_Listener_dump_enabled;
 class DataListener
   : public ConnectorDataListenerT<RTC::TimedLong>
 {
+  USE_CONNLISTENER_STATUS;
 public:
   DataListener(const char* name) : m_name(name) {}
   virtual ~DataListener()
@@ -46,8 +47,8 @@ public:
       }
   }
 
-  virtual void operator()(const ConnectorInfo& info,
-                          const TimedLong& data)
+  virtual ReturnCode operator()(ConnectorInfo& info,
+                                TimedLong& data)
   {
     // Connector Listener Dump check
     if(g_Listener_dump_enabled)
@@ -62,6 +63,7 @@ public:
         std::cout << "Data:          " << data.data    << std::endl;
         std::cout << "------------------------------"   << std::endl;
       }
+    return NO_CHANGE;
   }
   std::string m_name;
 };
@@ -70,6 +72,7 @@ public:
 class ConnListener
   : public ConnectorListener
 {
+  USE_CONNLISTENER_STATUS;
 public:
   ConnListener(const char* name) : m_name(name) {}
   virtual ~ConnListener()
@@ -81,7 +84,7 @@ public:
       }
   }
 
-  virtual void operator()(const ConnectorInfo& info)
+  virtual ReturnCode operator()(ConnectorInfo& info)
   {
     // Connector Listener Dump check
     if(g_Listener_dump_enabled)
@@ -95,6 +98,7 @@ public:
         std::cout                                       << std::endl;
         std::cout << "------------------------------"   << std::endl;
       }
+    return NO_CHANGE;
   };
   std::string m_name;
 };
