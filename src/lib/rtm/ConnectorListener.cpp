def78b5879c417162563fdc4cbfc3c12c5fe5f27
n-ando <Noriaki.Ando@gmail.com>
2017-12-16 08:50:45 +0000

    [compat,vc2008] stdint.h->cstdint and workaround for vc2008 added.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/branches/RELENG_1_2@3156 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

--- src/lib/rtm/ConnectorListener.cpp
+++ src/lib/rtm/ConnectorListener.cpp
@@ -18,7 +18,16 @@
  */
 
 #include <rtm/ConnectorListener.h>
-#include <stdint.h>
+
+// cstdint
+#if defined (_MSC_VER) && (_MSC_VER <=1500) // VC2008(VC9.0) or before
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <cstdint>
+#endif
 
 namespace RTC
 {
