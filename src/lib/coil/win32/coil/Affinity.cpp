7428e1df3c4f1898a95811d85cfd7420dfbe7809
Nobu19800 <openrtm@openrtm.org>
2018-03-28 03:51:07 +0000

    [merge] r3178-3180 have been merged from RELENG_1_2.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist/trunk@3270 8bdb766d-82ee-4cb2-b2de-c55db819e4d5

---
+++ src/lib/coil/win32/coil/Affinity.cpp
@@ -0,0 +1,121 @@
+﻿// -*- C++ -*-
+/*!
+ * @file Affinity.cpp
+ * @brief Processor affinity operation class
+ * @date $Date$
+ * @author Nobuhiko Miyamoto <n-miyamoto@aist.go.jp>
+ *
+ * Copyright (C) 2018
+ *     Nobuhiko Miyamoto
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id$
+ *
+ */
+
+
+#include <windows.h>
+#include <coil/stringutil.h>
+#include <coil/Affinity.h>
+
+namespace coil
+{
+  DWORD listToCUPNUM(CpuMask &cpu_mask)
+  {
+    DWORD cpu_num = 0;
+    for(CpuMask::iterator itr = cpu_mask.begin(); itr != cpu_mask.end(); ++itr) {
+      DWORD_PTR p = 0x01 << (*itr);
+      cpu_num += p;
+    }
+    return cpu_num;
+  }
+  bool getProcCpuAffinity(CpuMask& cpu_mask)
+  {
+	  DWORD cpu_num = listToCUPNUM(cpu_mask);
+    DWORD processMask, systemMask = 0;
+    HANDLE h = GetCurrentProcess();
+	BOOL success = GetProcessAffinityMask(h, &processMask, &systemMask);
+	if (success)
+    {
+		for (int i = 0; i < 32; i++)
+		{
+			if (processMask & (0x00000001 << i))
+			{
+				cpu_mask.push_back(i);
+			}
+		}
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  bool setProcCpuAffinity(std::vector<unsigned int> mask)
+  {
+	  DWORD cpu_num = listToCUPNUM(mask);
+    HANDLE h = GetCurrentProcess();
+    BOOL success = SetProcessAffinityMask(h, cpu_num);
+	if (success)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+  }
+
+  bool setProcCpuAffinity(std::string cpu_mask)
+  {
+    coil::vstring tmp = coil::split(cpu_mask, ",", true);
+    CpuMask mask;
+    for (size_t i(0); i < tmp.size(); ++i)
+      {
+        int num;
+        if (coil::stringTo(num, tmp[i].c_str()))
+          {
+            mask.push_back(num);
+          }
+      }
+    return setProcCpuAffinity(mask);
+  }
+
+  bool getThreadCpuAffinity(CpuMask& cpu_mask)
+  {
+    return true;
+  }
+
+  bool setThreadCpuAffinity(std::vector<unsigned int> mask)
+  {
+	  DWORD cpu_num = listToCUPNUM(mask);
+    HANDLE h = GetCurrentThread();
+    BOOL success = SetThreadAffinityMask(h, cpu_num);
+	if (success)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+  }
+
+  bool setThreadCpuAffinity(std::string cpu_mask)
+  {
+    coil::vstring tmp = coil::split(cpu_mask, ",", true);
+    CpuMask mask;
+    for (size_t i(0); i < tmp.size(); ++i)
+      {
+        int num;
+        if (coil::stringTo(num, tmp[i].c_str()))
+          {
+            mask.push_back(num);
+          }
+      }
+    return setThreadCpuAffinity(mask);
+  }
+}; // namespace coil
