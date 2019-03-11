851b34cb9f1cfc89aeee17036cc8b9cdb337fddd
Noriaki Ando <Noriaki.Ando@gmail.com>
2019-03-11 16:40:41 +0900

    Update ConfigAdmin.cpp
    leaf[i] -> prop because of new for loop.

--- src/lib/rtm/ConfigAdmin.cpp
+++ src/lib/rtm/ConfigAdmin.cpp
@@ -102,7 +102,7 @@ namespace RTC
 
     for (auto prop : leaf)
       {
-        if (leaf[i]->hasKey(param_name) != nullptr)
+        if (prop->hasKey(param_name) != nullptr)
           {
             coil::Properties* p(prop->removeNode(param_name));
             delete p;
