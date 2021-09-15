26f9b3112b27e31ff4671eb02c0b5991815589cb
n-kawauchi <n.kawauchi@aist.go.jp>
2021-09-15 10:38:05 +0900

    Regenerated Controller, Motor and Sensor using cmake.

---
+++ examples/Controller/test/src/ControllerTest.cpp
@@ -0,0 +1,165 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  ControllerTest.cpp
+ * @brief Controller component (test code)
+ *
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ */
+// </rtc-template>
+
+#include "ControllerTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* const controller_spec[] =
+  {
+    "implementation_id", "ControllerTest",
+    "type_name",         "ControllerTest",
+    "description",       "Controller component",
+    "version",           "2.0.0",
+    "vendor",            "AIST",
+    "category",          "example",
+    "activity_type",     "SPORADIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ControllerTest::ControllerTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_inOut("in", m_in),
+    m_outIn("out", m_out)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ControllerTest::~ControllerTest()
+{
+}
+
+
+
+RTC::ReturnCode_t ControllerTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("out", m_outIn);
+  
+  // Set OutPort buffer
+  addOutPort("in", m_inOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ControllerTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t ControllerTest::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onActivated(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onDeactivated(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t ControllerTest::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t ControllerTest::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ControllerTest::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+bool ControllerTest::runTest()
+{
+    return true;
+}
+
+
+extern "C"
+{
+ 
+  void ControllerTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(controller_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ControllerTest>,
+                             RTC::Delete<ControllerTest>);
+  }
+  
+}
---
+++ examples/Motor/test/src/MotorTest.cpp
@@ -0,0 +1,176 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  MotorTest.cpp
+ * @brief Motor component (test code)
+ *
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ */
+// </rtc-template>
+
+#include "MotorTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* const motor_spec[] =
+  {
+    "implementation_id", "MotorTest",
+    "type_name",         "MotorTest",
+    "description",       "Motor component",
+    "version",           "2.0.0",
+    "vendor",            "AIST",
+    "category",          "example",
+    "activity_type",     "SPORADIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.motor_id", "0",
+
+    // Widget
+    "conf.__widget__.motor_id", "text",
+    // Constraints
+
+    "conf.__type__.motor_id", "int",
+
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+MotorTest::MotorTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_inOut("in", m_in),
+    m_outIn("out", m_out)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+MotorTest::~MotorTest()
+{
+}
+
+
+
+RTC::ReturnCode_t MotorTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("out", m_outIn);
+  
+  // Set OutPort buffer
+  addOutPort("in", m_inOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("motor_id", m_motor_id, "0");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t MotorTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t MotorTest::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onActivated(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onDeactivated(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t MotorTest::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t MotorTest::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t MotorTest::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+bool MotorTest::runTest()
+{
+    return true;
+}
+
+
+extern "C"
+{
+ 
+  void MotorTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(motor_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<MotorTest>,
+                             RTC::Delete<MotorTest>);
+  }
+  
+}
