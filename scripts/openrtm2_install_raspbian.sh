c3640d10955765a05bf979dcdd206bb4a95c1cbb
n-kawauchi <n.kawauchi@aist.go.jp>
2022-08-02 14:45:42 +0900

    Updated 1.2 and 2.0 version of installation script for raspbian.

--- scripts/openrtm2_install_raspbian.sh
+++ scripts/openrtm2_install_raspbian.sh
@@ -6,8 +6,8 @@
 #         Nobu Kawauchi
 #
 
-VERSION=2.0.0.04
-FILENAME=pkg_install_raspbian.sh
+VERSION=2.0.0.01
+FILENAME=openrtm2_install_raspbian.sh
 
 #---------------------------------------
 # usage
@@ -17,9 +17,9 @@ usage()
   cat <<EOF
   Usage: 
 
-    $(FILENAME) -l {all|c++} [-r|-d|-s|-c] [-t OpenRTM-aist old version number] [-u|--yes]
+    $(FILENAME) -l {all|c++} [-r|-d|-s|-c] [-u|--yes]
     $(FILENAME) [-u]
-    $(FILENAME) -l {python} [-r|-d|-c] [-t OpenRTM-aist old version number] [-u|--yes]
+    $(FILENAME) -l {python} [-r|-d|-c] [-u|--yes]
     $(FILENAME) -l {java} [-r|-d|-c] [-u|--yes]
     $(FILENAME) -l {rtshell} [-d] [-u|--yes]
     $(FILENAME) {--help|-h|--version}
@@ -29,14 +29,12 @@ usage()
     $(FILENAME) -l all -d
     $(FILENAME) -l c++ -c --yes
     $(FILENAME) -l all -u
-    $(FILENAME) -l all -d -t 1.2.1
 
   Options:
     -l <argument>  language or tool [c++|python|java|rtshell|all]
         all        install packages of all the supported languages and tools
     -r             install robot component runtime
     -d             install robot component developer [default]
-    -t <argument>  OpenRTM-aist old version number
     -s             install tool_packages for build source packages
     -c             install tool_packages for core developer
     -u             uninstall packages
@@ -58,14 +56,8 @@ default_reposerver="openrtm.org"
 reposervers="openrtm.org"
 reposerver=""
 
-RTM_OLD_VER=1.2.1
-old_openrtm_devel="openrtm-aist-doc=$RTM_OLD_VER-0 openrtm-aist-idl=$RTM_OLD_VER-0 openrtm-aist-dev=$RTM_OLD_VER-0"
-old_openrtm_runtime="openrtm-aist=$RTM_OLD_VER-0 openrtm-aist-example=$RTM_OLD_VER-0"
-old_openrtm_py_devel="openrtm-aist-python-doc=$RTM_OLD_VER-0"
-old_openrtm_py_runtime="openrtm-aist-python=$RTM_OLD_VER-0 openrtm-aist-python-example=$RTM_OLD_VER-0"
-
-openrtm2_py="openrtm2-python3-doc openrtm2-python3-example openrtm2-python3"
-openrtm2_java="openrtm2-java-doc openrtm2-java-example openrtm2-java"
+openrtm_py="openrtm-aist-python3-doc openrtm-aist-python3-example openrtm-aist-python3"
+openrtm_java="openrtm-aist-java-doc openrtm-aist-java-example openrtm-aist-java"
 
 #--------------------------------------- C++
 autotools="autoconf libtool libtool-bin"
@@ -76,22 +68,23 @@ cmake_tools="cmake doxygen"
 build_tools="subversion git"
 deb_pkg="uuid-dev libboost-filesystem-dev"
 pkg_tools="build-essential debhelper devscripts"
+fluentbit="td-agent-bit"
 omni_devel="libomniorb4-dev omniidl"
 omni_runtime="omniorb-nameserver"
-openrtm_devel="openrtm-aist-doc openrtm-aist-idl openrtm-aist-dev"
-openrtm_runtime="openrtm-aist openrtm-aist-example"
+openrtm2_devel="openrtm2-doc openrtm2-idl openrtm2-dev"
+openrtm2_runtime="openrtm2 openrtm2-example"
 
 #--------------------------------------- Python
 omnipy="omniidl-python3"
 python_runtime="python3 python3-omniorb-omg"
 python_devel="python3-pip $cmake_tools $base_tools $omnipy $common_devel"
-openrtm_py_devel="openrtm-aist-python3-doc"
-openrtm_py_runtime="openrtm-aist-python3 openrtm-aist-python3-example"
+openrtm2_py_devel="openrtm2-python3-doc"
+openrtm2_py_runtime="openrtm2-python3 openrtm2-python3-example"
 
 #--------------------------------------- Java
 java_build="ant"
-openrtm_j_devel="openrtm-aist-java-doc"
-openrtm_j_runtime="openrtm-aist-java openrtm-aist-java-example"
+openrtm2_j_devel="openrtm2-java-doc"
+openrtm2_j_runtime="openrtm2-java openrtm2-java-example"
 
 
 #---------------------------------------
@@ -347,6 +340,7 @@ check_reposerver()
 #---------------------------------------
 create_srclist () {
   openrtm_repo="deb http://$reposerver/pub/Linux/raspbian/ $code_name main"
+  fluent_repo="deb https://packages.fluentbit.io/raspbian/$code_name $code_name main"
 }
 
 #---------------------------------------
@@ -367,14 +361,22 @@ update_source_list () {
       echo $openrtm_repo | sudo tee -a /etc/apt/sources.list
     fi
   fi
-
+  
   if test "x$FORCE_YES" = "xtrue" ; then
-    apt-get install --assume-yes --allow-unauthenticated dirmngr
+    sudo apt install --assume-yes --allow-unauthenticated dirmngr
   else
-    apt-get install dirmngr
+    sudo apt install dirmngr
   fi
+  fluentsite=`apt-cache policy | grep "https://packages.fluentbit.io"`
+  if test "x$fluentsite" = "x" &&
+     test "x$OPT_CORE" = "xtrue" ; then
+    echo $fluent_repo | sudo tee -a /etc/apt/sources.list
+  fi  
 　# 公開鍵登録
   wget -O- --secure-protocol=TLSv1_2 --no-check-certificate https://openrtm.org/pub/openrtm.key | sudo apt-key add -
+  if test "x$OPT_CORE" = "xtrue" ; then
+    wget -O - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
+  fi
 }
 
 #----------------------------------------
@@ -433,27 +435,27 @@ uninstall_packages () {
 }
 
 #-------------------------------------------------
-# If OpenRTM-aist 2.0 is installed, uninstall its.
-# Target: openrtm2-python3, openrtm2-java
+# If OpenRTM-aist 1.2 is installed, uninstall its.
+# Target: openrtm-aist-python3, openrtm-aist-java
 #         rtshell
 #-------------------------------------------------
-uninstall_openrtm20 ()
+uninstall_openrtm12 ()
 {
   if test "x$arg_python" = "xtrue" ; then
-    res=`dpkg -l | grep openrtm2-python3`
+    res=`dpkg -l | grep openrtm-aist-python3`
     if test ! "x$res" = "x" ; then
-      echo "Uninstall openrtm2-python3."
-      sudo apt --purge remove $openrtm2_py
-      uninstall_pkgs=$openrtm2_py
+      echo "Uninstall openrtm-aist-python3."
+      sudo apt --purge remove $openrtm_py
+      uninstall_pkgs=$openrtm_py
     fi
   fi
 
   if test "x$arg_java" = "xtrue" ; then
-    res=`dpkg -l | grep openrtm2-java`
+    res=`dpkg -l | grep openrtm-aist-java`
     if test ! "x$res" = "x" ; then
-      echo "Uninstall openrtm2-java."
-      sudo apt --purge remove $openrtm2_java
-      tmp_pkg="$uninstall_pkgs $openrtm2_java"
+      echo "Uninstall openrtm-aist-java."
+      sudo apt --purge remove $openrtm_java
+      tmp_pkg="$uninstall_pkgs $openrtm_java"
       uninstall_pkgs=$tmp_pkg
     fi
   fi
@@ -476,35 +478,35 @@ uninstall_openrtm20 ()
 set_package_content()
 {
 #--------------------------------------- C++
-runtime_pkgs="$omni_runtime $openrtm_runtime"
-u_runtime_pkgs=$openrtm_runtime
+runtime_pkgs="$omni_runtime $openrtm2_runtime"
+u_runtime_pkgs=$openrtm2_runtime
 
 src_pkgs="$cxx_devel $cmake_tools $deb_pkg $base_tools $omni_runtime $omni_devel"
 u_src_pkgs="$omni_runtime $omni_devel"
 
-dev_pkgs="$runtime_pkgs $src_pkgs $openrtm_devel"
-u_dev_pkgs="$u_runtime_pkgs $openrtm_devel"
+dev_pkgs="$runtime_pkgs $src_pkgs $openrtm2_devel"
+u_dev_pkgs="$u_runtime_pkgs $openrtm2_devel"
 
-core_pkgs="$src_pkgs $build_tools $pkg_tools"
+core_pkgs="$src_pkgs $build_tools $pkg_tools $fluentbit"
 u_core_pkgs="$u_src_pkgs"
 
 
 #--------------------------------------- Python
-python_runtime_pkgs="$omni_runtime $python_runtime $openrtm_py_runtime"
-u_python_runtime_pkgs="$openrtm_py_runtime"
+python_runtime_pkgs="$omni_runtime $python_runtime $openrtm2_py_runtime"
+u_python_runtime_pkgs="$openrtm2_py_runtime"
 
-python_dev_pkgs="$python_runtime_pkgs $python_devel $openrtm_py_devel"
-u_python_dev_pkgs="$u_python_runtime_pkgs $openrtm_py_devel"
+python_dev_pkgs="$python_runtime_pkgs $python_devel $openrtm2_py_devel"
+u_python_dev_pkgs="$u_python_runtime_pkgs $openrtm2_py_devel"
 
 python_core_pkgs="$omni_runtime $python_runtime $python_devel $build_tools $pkg_tools"
 u_python_core_pkgs="$omni_runtime $omnipy"
 
 #--------------------------------------- Java
-java_runtime_pkgs="$omni_runtime $openrtm_j_runtime"
-u_java_runtime_pkgs="$openrtm_j_runtime"
+java_runtime_pkgs="$omni_runtime $openrtm2_j_runtime"
+u_java_runtime_pkgs="$openrtm2_j_runtime"
 
-java_dev_pkgs="$java_runtime_pkgs $cmake_tools $base_tools $openrtm_j_devel"
-u_java_dev_pkgs="$u_java_runtime_pkgs $openrtm_j_devel"
+java_dev_pkgs="$java_runtime_pkgs $cmake_tools $base_tools $openrtm2_j_devel"
+u_java_dev_pkgs="$u_java_runtime_pkgs $openrtm2_j_devel"
 
 java_core_pkgs="$omni_runtime $cmake_tools $base_tools $build_tools $java_build $pkg_tools"
 u_java_core_pkgs="$omni_runtime"
@@ -515,6 +517,8 @@ u_java_core_pkgs="$omni_runtime"
 #---------------------------------------
 install_proc()
 {
+  local msg tmp
+
   if test "x$arg_cxx" = "xtrue" ; then
     if test "x$OPT_CORE" = "xtrue" ; then
       select_opt_c="[c++] install tool_packages for core developer"
@@ -526,6 +530,7 @@ install_proc()
       select_opt_c="[c++] install robot component runtime"
       install_packages $runtime_pkgs
     else
+      OPT_DEV=true
       select_opt_c="[c++] install robot component developer"
       install_packages $dev_pkgs
     fi
@@ -535,6 +540,9 @@ install_proc()
     if test "x$OPT_CORE" = "xtrue" ; then
       select_opt_p="[python] install tool_packages for core developer"
       install_packages $python_core_pkgs
+      pip3 install fluent-logger
+      tmp_pkg="$install_pkgs fluent-logger"
+      install_pkgs=$tmp_pkg
     elif test "x$OPT_RT" = "xtrue" ; then
       select_opt_p="[python] install robot component runtime"
       install_packages $python_runtime_pkgs
@@ -576,6 +584,8 @@ install_proc()
 #---------------------------------------
 uninstall_proc()
 {
+  local msg tmp
+
   if test "x$arg_cxx" = "xtrue" ; then
     if test "x$OPT_CORE" = "xtrue" ; then
       select_opt_c="[c++] uninstall tool_packages for core developer"
@@ -587,6 +597,7 @@ uninstall_proc()
       select_opt_c="[c++] uninstall robot component runtime"
       uninstall_packages `reverse $u_runtime_pkgs`
     else
+      OPT_DEV=true
       select_opt_c="[c++] uninstall robot component developer"
       uninstall_packages `reverse $u_dev_pkgs`
     fi
@@ -756,17 +767,22 @@ fi
 set_package_content
 
 if test "x$OPT_FLG" = "xtrue" ; then
-  uninstall_openrtm20
+  uninstall_openrtm12
   install_proc
 else
   uninstall_proc
 fi
 
 # install openjdk-8-jdk
-if test "x$OPT_UNINST" = "xtrue" ; then
+if test "x$OPT_FLG" = "xtrue" ; then
   sudo apt -y install openjdk-8-jdk
   JAVA8=`update-alternatives --list java | grep java-8`
   sudo update-alternatives --set java ${JAVA8}
+fi
+
+if test "x$OPT_CORE" = "xtrue" ; then
+  systemctl enable td-agent-bit
+  systemctl start td-agent-bit
 fi
 
 install_result $install_pkgs
@@ -777,7 +793,19 @@ if test ! "x$err_message" = "x" ; then
   echo "${ESC}[33m${err_message}${ESC}[m"
 fi
 
-if test "x$OPT_UNINST" = "xfalse" ; then
+ESC=$(printf '\033')
+if test "x$OPT_FLG" = "xtrue" &&
+   test "x$arg_cxx" = "xtrue" &&
+   test "x$OPT_COREDEVEL" = "xfalse" ; then
+  msg1='To use the log collection extension using the Fluentd logger,'
+  msg2='please install Fluent Bit by following the steps on the following web page.'
+  msg3='https://docs.fluentbit.io/manual/installation/linux/raspbian-raspberry-pi'
+  echo $LF
+  echo "${ESC}[33m${msg1}${ESC}[m"
+  echo "${ESC}[33m${msg2}${ESC}[m"
+  echo "${ESC}[33m${msg3}${ESC}[m"
+fi
+if test "x$OPT_FLG" = "xfalse" ; then
   ESC=$(printf '\033')
   msg1='omniorb or other OpenRTM dependent packages may still exist. '
   msg2='If you want to remove them, please do “apt autoremove” later.'
