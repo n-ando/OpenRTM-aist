75b6e83e22a5f34e29bcfe0aea81f968f7187abe
Noriaki Ando <Noriaki.Ando@gmail.com>
2023-02-27 17:07:51 +0900

    Remove inline direective in private functions
    Inline directives in the private functions of the RingBuffer class template have been removed.
    When compilation by g++ 11.3 with the release option, RingBuffer instantiation fails with the following errors.
    
    ```
    RingBuffer.h:846:17: error: inlining failed in call to ‘void RTC::RingBuffer<DataType>::initLength(const coil::Properties&) [with DataType = RTC::ByteData]’: --param max-inline-insns-single limit reached [-Werror=inline]
      846 |     inline void initLength(const coil::Properties& prop)
    ```

--- src/lib/rtm/RingBuffer.h
+++ src/lib/rtm/RingBuffer.h
@@ -843,7 +843,7 @@ namespace RTC
     }
 
   private:
-    inline void initLength(const coil::Properties& prop)
+    void initLength(const coil::Properties& prop)
     {
       if (!prop["length"].empty())
         {
@@ -858,7 +858,7 @@ namespace RTC
         }
     }
 
-    inline void initWritePolicy(const coil::Properties& prop)
+    void initWritePolicy(const coil::Properties& prop)
     {
       std::string policy(coil::normalize(prop["write.full_policy"]));
       if (policy == "overwrite")
@@ -885,7 +885,7 @@ namespace RTC
         }
     }
 
-    inline void initReadPolicy(const coil::Properties& prop)
+    void initReadPolicy(const coil::Properties& prop)
     {
       std::string policy(prop["read.empty_policy"]);
       if (policy == "readback")
